e5124359a344187d14981b1c05940e46fcce53a3
Jonas Enge <jonas.enge@gmail.com>
2020-02-19 23:05:12 +0100

    Genesis commit

---
+++ api/compare.js
@@ -0,0 +1,33 @@
+const { ObjectId } = require('mongodb')
+const connectToDb = require('./lib/connectToDb')
+const repackResults = require('./lib/repack-results')
+const base64url = require('./lib/base64url')
+
+module.exports = async (req, res) => {
+  const { query: { id } } = req
+
+  if (!id) {
+    res.status(500).json({ type: 'error', message: 'Not a valid id' })
+    return
+  }
+
+  const people = base64url.decode(id)
+
+  try {
+    const db = await connectToDb()
+    const collection = db.collection('test')
+    const scores = await Promise.all(
+      people.map(async item => {
+        const data = await collection.findOne({ _id: ObjectId(item.id) })
+        return { data, name: item.name }
+      })
+    )
+
+    const results = repackResults(scores, scores[0].data.lang)
+
+    res.json(results)
+    return
+  } catch (error) {
+    res.status(500).json({ type: 'error', message: error.message })
+  }
+}
