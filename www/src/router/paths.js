37faaa93f9a05633588bf0b3bb4888ee1c6a64b9
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-02-22 21:30:52 +0100

    Implements sitemap.xml (patch)

---
+++ www/src/router/paths.js
@@ -0,0 +1,52 @@
+const paths = [
+  {
+    path: '/',
+    name: 'Home'
+  },
+  {
+    path: '/form',
+    name: 'Form'
+  },
+  {
+    path: '/test',
+    name: 'test'
+  },
+  {
+    path: '/result/:id',
+    name: 'result'
+  },
+  {
+    path: '/compare/:id',
+    name: 'compare'
+  },
+  {
+    path: '/bigfive',
+    name: 'bigfive'
+  },
+  {
+    path: '/openness_to_experience',
+    name: 'openness'
+  },
+  {
+    path: '/conscientiousness',
+    name: 'conscientiousness'
+  },
+  {
+    path: '/extraversion',
+    name: 'extraversion'
+  },
+  {
+    path: '/agreeableness',
+    name: 'agreeableness'
+  },
+  {
+    path: '/neuroticism',
+    name: 'neuroticism'
+  },
+  {
+    path: '/about',
+    name: 'about'
+  }
+]
+
+module.exports = { default: paths }
--- www/vue.config.js
+++ www/vue.config.js
@@ -1,6 +1,17 @@
 const manifestJSON = require('./public/manifest.json')
+const SitemapPlugin = require('sitemap-webpack-plugin').default
+const routerPaths = require('./src/router/paths').default.map(r => r.path).filter(p => !p.match(/\*/))
 
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new SitemapPlugin('https://www.bigfive-test.com', routerPaths, {
+        fileName: 'sitemap.xml',
+        lastMod: true,
+        changeFreq: 'weekly'
+      })
+    ]
+  },
   pwa: {
     themeColor: manifestJSON.theme_color,
     workboxOptions: {
