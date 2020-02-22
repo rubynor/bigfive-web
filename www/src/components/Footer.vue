4f6584191570a343b8c7e443140884f9438ab69e
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2020-02-22 18:46:31 +0100

    comment out footer, not ready for production

--- www/src/components/Footer.vue
+++ www/src/components/Footer.vue
@@ -1,27 +1,27 @@
 <template>
   <v-footer class="pl-10">
-    <v-col class="footer-heading">
-      SAY<br/>HALLO
-    </v-col>
-    <v-col cols="12" class="pt-0">
-      <v-icon>mdi-facebook</v-icon>
-      <v-divider inset vertical class="mx-1"></v-divider>
-      <v-icon>mdi-twitter</v-icon>
-      <v-divider inset vertical class="mx-1"></v-divider>
-      <v-icon>mdi-linkedin</v-icon>
-    </v-col>
-    <v-col cols="12" class="footer-about">
-      Rubynor<br/>
-      Bedriftsveien 64<br/>
-      3735 Skien, Norway<br /><br />
-      hello@bigfive-test.com<br />
-    </v-col>
-    <v-col
-      class="footer-text"
-      cols="12"
-    >
-      © {{ new Date().getFullYear() }} — Rubynor - all rights reserved.
-    </v-col>
+<!--    <v-col class="footer-heading">-->
+<!--      SAY<br/>HALLO-->
+<!--    </v-col>-->
+<!--    <v-col cols="12" class="pt-0">-->
+<!--      <v-icon>mdi-facebook</v-icon>-->
+<!--      <v-divider inset vertical class="mx-1"></v-divider>-->
+<!--      <v-icon>mdi-twitter</v-icon>-->
+<!--      <v-divider inset vertical class="mx-1"></v-divider>-->
+<!--      <v-icon>mdi-linkedin</v-icon>-->
+<!--    </v-col>-->
+<!--    <v-col cols="12" class="footer-about">-->
+<!--      Rubynor<br/>-->
+<!--      Bedriftsveien 64<br/>-->
+<!--      3735 Skien, Norway<br /><br />-->
+<!--      hello@bigfive-test.com<br />-->
+<!--    </v-col>-->
+<!--    <v-col-->
+<!--      class="footer-text"-->
+<!--      cols="12"-->
+<!--    >-->
+<!--      © {{ new Date().getFullYear() }} — Rubynor - all rights reserved.-->
+<!--    </v-col>-->
   </v-footer>
 </template>
 
