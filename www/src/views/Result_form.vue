780441f5bddcf8127c10caf7dff51880376f55e3
Jonas Enge <jonas.enge@gmail.com>
2020-02-23 21:58:16 +0100

    Adds compare/result

--- www/src/views/Result_form.vue
+++ www/src/views/Result_form.vue
@@ -5,18 +5,20 @@
       If you have taken the test and saved your ID, you can see the results here by typing in the ID you got i.e. <span class="font-italic secondary--text">58a70606a835c400c8b38e84</span> in the ID-input field
       <br />
       <v-text-field
+        class="pt-5"
         v-model="id"
         label="ID"
         hide-details="auto"
-        :rules="[rules.counter]"
+        placeholder="URL or id of result"
+        :rules="[rules.valid]"
       ></v-text-field>
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn
           color="primary"
-          :to="'/result/' + id"
+          :to="'/result/' + this.formatId(id)"
           large
-          :disabled="!this.validMongoId(id)"
+          :disabled="!this.validMongoId(this.formatId(id))"
         >
           get result
         </v-btn>
@@ -26,17 +28,18 @@
 </template>
 
 <script>
-import { validMongoId } from '../lib/helpers.js'
+import { validMongoId, formatId } from '../lib/helpers.js'
 
 export default {
   name: 'ResultForm',
   methods: {
-    validMongoId: validMongoId
+    validMongoId: validMongoId,
+    formatId: formatId
   },
   data: () => ({
     id: '',
     rules: {
-      counter: value => validMongoId(value) || 'Not a valid ID'
+      valid: value => validMongoId(formatId(value)) || 'Not a valid ID'
     }
   })
 }
