caa39b975d5db2cb9084210ec8fe1fa92543e4b3
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-03-09 19:25:22 +0100

    Adds Amplitude for language switcher (patch)

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -4,6 +4,7 @@
     <br />
     <nuxt-link
       v-for="locale in availableLocales"
+      @click.native="logClick(locale.code)"
       :key="locale.code"
       :to="switchLocalePath(locale.code)">{{ locale.name }}
     </nuxt-link>
@@ -13,6 +14,11 @@
 <script>
 export default {
   name: 'LanguageSwitcher',
+  methods: {
+    logClick (code) {
+      this.$amplitude.getInstance().logEvent('b5.langauge.switch', { code })
+    }
+  },
   computed: {
     availableLocales () {
       return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
