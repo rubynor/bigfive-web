42c6e44ba9341ba8b352884bd36b1aebf425a08d
Jonas Enge <jonas.enge@gmail.com>
2020-03-09 19:29:02 +0100

    translations

--- translations/nb-NO.js
+++ translations/nb-NO.js
@@ -124,5 +124,109 @@ innholdet er fra <i>Johnson's (2014) 120-item IPIP NEO-PI-R</i>.`,
     shareTwitter: 'Del på Twitter',
     copiedLink: 'Kopierte lenke',
     shareResults: 'Del dine resultater!'
+  },
+  opennessToExperience: {
+    quote: 'Åpenhet for erfaringer beskriver en dimensjon av en kognitiv stil der fantasi skiller kreative mennesker fra jordnære, konvensjonelle mennesker.',
+    text1: `
+    <p>
+      Åpne mennesker er intellektuelt nysgjerrige, setter pris på kunst, og de verdsetter skjønnhet.
+    </p>
+    <p>
+      De pleier å være mer klar over sine følelser enn mennesker som er mer lukket.
+    </p>
+    <p>
+      De tenker og handler ofte på individuelle og lite konforme måter.
+      De intellektuelle skårer vanligvis høyere på åpenhet for erfaringer; derfor har denne faktoren også blitt kalt kultur eller intellekt.
+    </p>
+    <p>
+      Ikke desto mindre, blir intellektet ofte sett på som et aspekt ved åpenhet for erfaringer.
+      Skåren på åpenhet for erfaringer er bare i liten grad relatert til utdanning og skåren på standardiserte IQ-tester.
+    </p>
+    <p>
+      Et annet kjennetegn ved den åpne kognitive stilen er en evne til å tenke i symboler og abstraksjoner, også når de ikke har med konkret erfaring å gjøre.
+    </p>
+    <p>
+      Avhengig  av idividets spesifikke intellektulle evner, kan denne kognisjonen i forhold til symboler opptre i form av matematikk, logisk eller geometrisk
+      tenkning, kunstnerisk og metaforisk bruk av språk, musikalsk komposisjon eller
+      framføring, eller som en  av mange uttrykksformer innenfor visuell eller utøvende kunst.
+    </p>
+    <p>
+      Mennesker med lave skårer på åpenhet for erfaringer har gjerne begrensede, vanlige interesser.
+      De foretrekker det enkle, ukompliserte og opplagte framfor det komplekse, tvetydige og dype.
+    </p>
+    <p>
+      De kan se på kunst og vitenskap med skepsis og se på det disse forfekter som obskurt og ikke av noen nytteverdi.
+      Lukkede personligheter foretrekker det kjente framfor det ukjente; de er konservative og er ikke glad i forandringer.
+    </p>
+    <p>
+      Åpenhet blir ofte sett på som sunnere og mer modent av psykologer, kanskje fordi de ofte selv er åpne for erfaringer.
+      Men åpne og lukkede måter å tenke på kan være nyttig i ulike miljøer.
+    </p>
+    <p>
+      Den intellektuelle tenkemåten til en åpen person
+      kan fungere bra hos en professor, men forskning har vist at lukket tenkning kan relateres til framragende arbeid i politietaten, innenfor salg og i en rekke service-yrker.
+    </p>
+    `,
+    text2: `
+      <div class="title">
+        Fantasi
+      </div>
+      <p>
+        For individer med mye fantasi er den virkelige verden ofte for enkel og ordinær. De som skårer høyt her bruker fantasien for å skape seg en rikere og mer interessant verden. De som skårer lavt her er mer faktaorienterte.
+      </p>
+      <div class="title">
+        Estetikk
+      </div>
+      <p>
+        De som skårer høyt her elsker skjønnhet, både i kunsten og naturen. De lar seg lett rive med og begeistres av det kunstneriske og det som skjer i naturen.
+      </p>
+      <p>
+        De er ikke nødvendigvis talentfulle eller utdannet innenfor disse områdene, selv om mange vil være det. Trekkene som definerer denne skalaen er interesse for, og verdsettelse av naturlig og kunstnerisk skjønnhet.
+      <p>
+        De som skårer lavt mangler estetisk sans og interesse for kunst.
+      </p>
+      <div class="title">
+        Følelser
+      </div>
+      <p>
+        Personer som skårer høyt på følelser har god kontakt med sine egne følelser. De som skårer lavt er mindre oppmerksomme på egne følelser og vil ikke gi åpent uttrykk for dem.
+      </p>
+      <div class="title">
+        Eventyrlyst
+      </div>
+      <p>
+        De som skårer høyt på dette vil gjerne prøve nye aktiviteter, reise til fremmede land og oppleve nye ting. De synes rutine og det kjente er kjedelig og vil velge en annen vei hjem kun fordi den er annerledes.
+      </p>
+      <p>
+        De som skårer lavt liker dårlig forandring og foretrekker kjente rutiner.
+      </p>
+      <div class="title">
+        Intellekt
+      </div>
+      <p>
+        Intellekt og kunstnerisk sans er de to viktigeste og mest sentrale aspektene ved åpenhet for erfaringer. De som skårer høyt på intellekt liker å leke med tanker og idéer.
+      </p>
+      <p>
+        De er åpne ovenfor nye utradisjonelle idéer og liker å diskutere ulike intellektuelle problemer. De liker å løse gåter, puslespill og hjernetrim.
+      </p>
+      <p>
+        De som skårer lavt på dette foretrekker å ha med virkelige mennesker å gjøre framfor intellektuelle problemstillinger. De ser på intellektuelle aktiviteter som bortkastet tid. Intellekt burde ikke sammenstilles med intelligens.
+      </p>
+      <p>
+        Intellekt er en intellektuell stil, ikke en intellektuell evne, selv om de som skårer høyt på intellekt-skåren, skårer noe høyere enn de som er skårer lavt på intellekt på standardiserte intelligenstester.
+      </p>
+      <div class="title">
+        Liberale verdier
+      </div>
+      <p>
+        Denne psykologiske verdien når det gjelder liberalisme tar for seg tilbøyeligheten til å utfordre autoriteter, konvensjoner og tradisjonelle verdier.
+      </p>
+      <p>
+        I sin mest ekstreme form, kan denne psykologiske verdi-dimensjonen representere regelrett fiendtlighet ovenfor lover og og regler, sympati for lovbrytere og en forkjærlighet for det tvetydige, kaos og uorden.
+      </p>
+      <p>
+        De som er psykologisk konservative foretrekker trygghet og stabilitet ved å holde seg til tradisjoner. psykologisk liberalisme og konservatisme er ikke det samme som politisk tilhørighet, men er absolutt med å trekke individet mot visse politiske partier.
+      </p>
+    `
   }
 }
