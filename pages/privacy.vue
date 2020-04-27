05ab893fadfc6374ee6cea0ff7f308d9e2840f8a
Jonas Enge <jonas.enge@gmail.com>
2020-04-27 12:00:42 +0200

    Adds minimal privacy policy

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,9 +1,71 @@
 <template>
   <div>
-    <h1 class="subheading grey--text">Privacy</h1>
-    Under construction ...
+    <h1 class="subheading grey--text">Privacy Policy</h1>
+    This privacy policy will explain how out organization uses the personal data we collect from you
+    when you use our website.
+    <h2 class="subheading grey--text">What data do we collect?</h2>
+    We collect the following data
+    <br /><br />
+    <ul>
+      <li>
+        Age <span class="grey--text">(if submitted)</span>
+      </li>
+      <li>
+        Nationality <span class="grey--text">(if submitted)</span>
+      </li>
+      <li>
+        Gender <span class="grey--text">(if submitted)</span>
+      </li>
+      <li>
+        Choosen test language
+      </li>
+      <li>
+        Test answers
+      </li>
+      <li>
+        Datetime of submitted test
+      </li>
+    </ul>
+    <br />
+    We also use Google Analytics to measure traffic to our site and how users interact with our site. The Google Analytics terms specify that no personally identifiable information may be collected through the Google Analytics software.
+    <br /><br />
+    Read more about Google Analytics privacy policy <a href='https://policies.google.com/privacy' rel='noopener noreferrer' target='_blank'>here</a>.</p>
+    <h2 class="subheading grey--text">How do we collect your data?</h2>
+    Google Analytics sets the following cookies:
+    <br />
+    <br />
+    <ul>
+      <li>
+        "_ga" is used to distingusih users. The cookie is set the first time a user access the website and has a lifetime of 2 until years
+      </li>
+      <li>
+        "_gat" is used to limit the traffic from Google Analytics, this cookie has a lifetime of 10 minutes.
+      </li>
+    </ul>
+    <br />
+
+    Your web browser also automatically sends information to our hosting provider Zeit.
+    <br />
+    This can i.e. be information about wich browser and version you are using and your Internet address (IP-address). For each page that is displayed, the date and time information, which page you came from, which page you are on, etc. are also stored.
+    <br />
+    <br />
+    When you submit the test the information you provide (described above) is stored in our database.
+    <h2 class="subheading grey--text">How will we use your data?</h2>
+    We use Google Analytics to get statistics on how visitors use the site.
+    <br />
+    <br />
+    We will create statistical, aggregated data from the test results for analytical purposes.
+    <h2 class="subheading grey--text">How do we store your data?</h2>
+    <h2 class="subheading grey--text">How to contact us?</h2>
+    If you have any questions about this privacy policy, please <a href='mailto:support@bigfive-test.com' rel='noopener noreferrer' target='_blank'>contact</a> us.</p>
   </div>
 </template>
 
 <script>
 </script>
+<style scoped>
+h2 {
+  margin-top: 20px;
+  margin-bottom: 8px;
+}
+</style>
