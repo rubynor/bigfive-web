d4281b748dd81c1dc6a9dab986a245ab51576015
Jonas Enge <jonas.enge@gmail.com>
2020-05-16 14:16:16 +0200

    New design

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <v-app>
     <Toolbar />
 
-    <v-content v-if="$nuxt.$route.path === '/'">
+    <v-content v-if="$nuxt.$route.path === '/' || $nuxt.$route.path === '/no'">
       <nuxt />
     </v-content>
     <v-content v-else>
