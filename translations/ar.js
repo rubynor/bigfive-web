aff5b730f3a666ce5b9ab71a7250ed1c59e755b9
Jonas Enge <jonas.enge@gmail.com>
2020-05-24 23:27:16 +0200

    Arabic translation

---
+++ translations/ar.js
@@ -0,0 +1,327 @@
+export default {
+  language: 'Arabic',
+  seo: {
+    keywords: 'big five personality test, big 5 personality test, b5 test, bigfive test, personality traits, bigfive, compare, free, jordan peterson'
+  },
+  frontpage: {
+    seo: {
+      title: 'Free open-source BigFive personality traits test',
+      description: 'Take a free, open-source Big Five personality test. Learn to know your personality traits and compare yourself with your partner, colleagues, friends or family.'
+    },
+    title: 'Big Five Personality Test',
+    call_to_action: 'Take the free test now',
+    no_registration: '* No registration required',
+    tests_taken: 'Over 700.000 people has taken the test',
+    compare: {
+      title: 'Compare yourself to others',
+      text1: 'Compare yourself with your partner, colleagues, friends or family.',
+      text2: 'Learn how you differ from other people in the five domains:',
+      action: 'See it live in action'
+    },
+    share: 'Share with your friends and find out how compatible you are',
+    cards: {
+      open: {
+        title: 'Open',
+        text: 'This is an open source project under MIT-license.'
+      },
+      free: {
+        title: 'Free',
+        text: 'The test is completely free'
+      },
+      scientific: {
+        title: 'Scientific',
+        text: 'BigFive is a scientifically validated and reliable psychological model.'
+      },
+      translated: {
+        title: 'Translated',
+        text: 'Translated to over 20 languages. Help out!'
+      }
+    },
+    description: {
+      top: 'Learn to know yourself better with a free, open-source personality test.',
+      subtop: 'The',
+      subtop2: ' is the most scientifically validated and reliable psychological model to measure personality.',
+      reference: `Tests and evaluation for this site is gathered from
+<a href="http://ipip.ori.org" rel="noopener" target="_blank">ipip.ori.org</a>,
+inventory is from <i>Johnson's (2014) 120-item IPIP NEO-PI-R</i>.`,
+      info: 'The following test contains 120 questions which is estimated to take you about 10 minutes to complete.',
+      result: 'After you have completed the test you will receive a detailed personality report in the following domains:',
+      tests_taken: 'tests taken so far'
+    }
+  },
+  about: {
+    seo: {
+      title: 'About and the big five team',
+      description: 'BigFive has a very active and engaged team that is constantly striving to push BigFive forward.'
+    }
+  },
+  toolbar: {
+    home: 'Home',
+    result: 'Result',
+    compare: 'Compare',
+    articles: 'Articles',
+    privacy: 'Privacy',
+    about: 'About',
+    see_results: 'See your results',
+    compare_with: 'Compare with others'
+  },
+  facets: {
+    openness_to_experience: {
+      title: 'Openness to experience'
+    },
+    conscientiousness: {
+      title: 'Conscientiousness'
+    },
+    extraversion: {
+      title: 'Extraversion'
+    },
+    agreeableness: {
+      title: 'Agreeableness'
+    },
+    neuroticism: {
+      title: 'Neuroticism'
+    }
+  },
+  common: {
+    and: 'and',
+    pages: 'pages',
+    languages: 'languages'
+  },
+  form: {
+    information: 'Information',
+    informationText: 'We use the information you provide to serve you the',
+    mostAccurate: 'most accurate result possible',
+    readMoreAbout: 'Read more about',
+    privacy: 'privacy',
+    nextButton: 'Okey, next question',
+    declineButton: 'continue without sharing',
+    prefferedLanguage: 'My preffered language is',
+    selectLanguage: 'Select a language',
+    iama: 'I am a',
+    male: 'Male',
+    female: 'Female',
+    age: 'Age',
+    iam: 'I am',
+    yearsOld: 'years old',
+    ageWarning: 'You must be 16 or older',
+    language: 'Language',
+    gender: 'Gender',
+    confirmInfo: 'Confirm information',
+    confirm: 'Confirm',
+    toTestButton: 'Alrighty, take me to the test'
+  },
+  test: {
+    next: 'next',
+    back: 'back',
+    more: 'more',
+    seeResults: 'see results'
+  },
+  big_five: {
+    title: 'What are the big five?',
+    seo: {
+      title: 'What are the big five?',
+      description: 'Read more about the big five'
+    }
+  },
+  openness_to_experience: {
+    title: 'Openness to Experience',
+    seo: {
+      title: 'Overview of the domain Openness To Experience.',
+      description: 'Read more about the Openness To Experience domain in the b5 model'
+    }
+  },
+  conscientiousness: {
+    title: 'Conscientiousness',
+    seo: {
+      title: 'Overview of the domain Conscientiousness',
+      description: 'Read more about the Conscientiousness domain in the b5 model'
+    }
+  },
+  extraversion: {
+    title: 'Extraversion',
+    seo: {
+      title: 'Overview of the domain Extraversion',
+      description: 'Read more about the Extraversion domain in the b5 model'
+    }
+  },
+  agreeableness: {
+    title: 'Agreeableness',
+    seo: {
+      title: 'Overview of the domain Agreeableness',
+      description: 'Read more about the agreeableness domain in the b5 model'
+    }
+  },
+  neuroticism: {
+    title: 'Neuroticism',
+    seo: {
+      title: 'Overview of the domain Neuroticism',
+      description: 'Read more about the Neuroticism domain in the b5 model'
+    }
+  },
+  getCompare: {
+    title: 'Compare people or teams',
+    description1: 'Compare results from the bigfive personality test with multiple people.',
+    description2: 'Type in the ID you got from the test results i.e.',
+    description3: 'in the ID-input field',
+    needToAddPeople: 'You need to add people to compare them',
+    addAnother: 'Add another person',
+    addPerson: 'Add Person',
+    comparePeople: 'compare people',
+    nameOfPerson: 'Name of this person',
+    urlOrId: 'URL or ID for comparison',
+    name: 'Name'
+  },
+  results: {
+    theBigFive: 'The Big Five',
+    score: 'score',
+    important: 'Important!',
+    saveResults: 'Save the following ID to see the results later or ',
+    compare: 'compare',
+    toOthers: 'yourself to others',
+    seo: {
+      title: 'My results from the BigFive personality test',
+      description: 'See my personality and compare yourself in the following five domains: Neuroticism, Openness To Experience, Conscientiousness, Extraversion, Agreeableness'
+    }
+  },
+  getResult: {
+    result: 'Result',
+    explaination: 'If you have taken the test and saved your ID, you can see the results here by typing in the ID you got i.e. ',
+    idInput: 'in the ID-input field',
+    urlOrId: 'URL or ID of your result',
+    getResult: 'Get result'
+  },
+  shareLinks: {
+    copyLink: 'Copy link to clipboard',
+    shareFacebook: 'Share on Facebook',
+    shareTwitter: 'Share on Twitter',
+    copiedLink: 'Copied link',
+    shareResults: 'Share your results!'
+  },
+  opennessToExperience: {
+    quote: 'Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.',
+    text1: `
+      <p>
+        Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.
+      </p>
+      <p>
+        Open people are <b>intellectually curious, appreciative of art, and sensitive to beauty</b>.
+        They tend to be, compared to closed people, more <b>aware of their feelings</b>.
+        They tend to think and act in individualistic and nonconforming ways.
+      </p>
+      <p>
+        Intellectuals typically score high on Openness to Experience; consequently, this factor has also been called Culture or Intellect.
+      </p>
+    `,
+    text2: `
+      <p>
+        Nonetheless, Intellect is probably best regarded as one aspect of openness to experience.
+        Scores on Openness to Experience are only modestly related to years of education and scores on standard intelligent tests.
+      </p>
+      <p>
+        Another characteristic of the open cognitive style is a facility for thinking in symbols and abstractions far removed from concrete experience.
+      </p>
+      <p>
+        Depending on the individual's specific intellectual abilities, this symbolic cognition may take the form of mathematical, logical, or geometric thinking, artistic and metaphorical use of language, music composition or performance, or one of the many visual or performing arts.
+      </p>
+      <div class="title">
+        Low scores
+      </div>
+      <p>
+        People with <b>low scores</b> on openness to experience tend to have <b>narrow, common interests</b>.
+      </p>
+      <p>
+        They prefer the <b>plain, straightforward</b>, and obvious over the complex, ambiguous, and subtle.
+      </p>
+      <p>
+        They may regard the arts and sciences with suspicion, regarding these endeavors as abstruse or of no practical use.
+      </p>
+      <p>
+        Closed people prefer familiarity over novelty; they are conservative and resistant to change.
+      </p>
+      <p>
+        Openness is often presented as healthier or more mature by psychologists, who are often themselves open to experience. However, open and closed styles of thinking are useful in different environments.
+      </p>
+      <p>
+      The intellectual style of the open person may serve a professor well, but research has shown that closed thinking is related to superior job performance in police work, sales, and a number of service occupations.
+      </p>
+
+      <div class="title">
+        Imagination
+      </div>
+      <p>
+        To imaginative individuals, the real world is often too plain and ordinary.
+      </p>
+      <p>
+        <b>High scorers</b> on this scale use fantasy as a way of creating a richer, more interesting world.
+      </p>
+      <p>
+      <b>Low scorers</b> are on this scale are more oriented to facts than fantasy.
+      </p>
+
+      <div class="title">
+        Artistic Interests
+      </div>
+      <p>
+      <b>High scorers</b> on this scale love beauty, both in art and in nature. They become easily involved and absorbed in artistic and natural events.
+      </p>
+      <p>
+        They are not necessarily artistically trained nor talented, although many will be. The defining features of this scale are interest in, and appreciation of natural and artificial beauty.
+      </p>
+      <p>
+        <b>Low scorers</b> lack aesthetic sensitivity and interest in the arts.
+      </p>
+
+      <div class="title">
+        Emotionality
+      </div>
+      <p>
+        Persons high on Emotionality have good access to and awareness of their own feelings.
+      </p>
+      <p>
+        Low scorers are less aware of their feelings and tend not to express their emotions openly.
+      </p>
+
+      <div class="title">
+        Adventurousness
+      </div>
+      <p>
+        High scorers on adventurousness are eager to try new activities, travel to foreign lands, and experience different things.
+      </p>
+      <p>
+        They find familiarity and routine boring, and will take a new route home just because it is different.
+      </p>
+      <p>
+        Low scorers tend to feel uncomfortable with change and prefer familiar routines.
+      </p>
+
+      <div class="title">
+        Intellect
+      </div>
+      <p>
+        Intellect and artistic interests are the two most important, central aspects of openness to experience. High scorers on Intellect love to play with ideas.
+      </p>
+      <p>
+        They are open-minded to new and unusual ideas, and like to debate intellectual issues. They enjoy riddles, puzzles, and brain teasers. Low scorers on Intellect prefer dealing with either people or things rather than ideas.
+      </p>
+      <p>
+        They regard intellectual exercises as a waste of time.
+      </p>
+      <p>
+        Intellect should not be equated with intelligence. Intellect is an intellectual style, not an intellectual ability, although high scorers on Intellect score slightly higher than low-Intellect individuals on standardized intelligence tests.
+      </p>
+
+      <div class="title">
+        Liberalism
+      </div>
+      <p>
+        Psychological liberalism refers to a readiness to challenge authority, convention, and traditional values.
+      </p>
+      <p>
+        In its most extreme form, psychological liberalism can even represent outright hostility toward rules, sympathy for law-breakers, and love of ambiguity, chaos, and disorder.
+      </p>
+      <p>
+        Psychological conservatives prefer the security and stability brought by conformity to tradition. Psychological liberalism and conservatism are not identical to political affiliation, but certainly incline individuals toward certain political parties.
+      </p>
+    `
+  }
+}
