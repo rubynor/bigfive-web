96d00d66527138e10f1eec87870b6274b5ab14cd
Jonas Enge <jonas.enge@gmail.com>
2020-10-06 00:50:49 +0200

    Adds FAQ

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="faq">
+    <v-list class="d-none">
+      <v-list-item
+        v-for="link of page.toc"
+        :key="link.id"
+        class
+      >
+        <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+      </v-list-item>
+    </v-list>
     <nuxt-content :document="page" />
   </div>
 </template>
@@ -21,5 +30,9 @@ export default {
 
 .nuxt-content h3 {
   margin-bottom: 8px;
+}
+
+.nuxt-content ul {
+  list-style: none;
 }
 </style>
