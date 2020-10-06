a5018fbd161cb3dace025fd159c0541cbf4078d0
Shubham Kabra <shubhamkabra6298@gmail.com>
2020-10-06 20:31:06 +0530

    Adding support for Indonesian language

---
+++ translations/id.js
@@ -0,0 +1,327 @@
+export default {
+    language: 'Bahasa Indonesia',
+    seo: {
+        keywords: 'tes kepribadian lima besar, tes kepribadian 5 besar, tes b5, tes bigfive, ciri-ciri kepribadian, bigfive, bandingkan, gratis, jordan peterson'
+    },
+    frontpage: {
+        seo: {
+            title: 'Tes sifat kepribadian BigFive open-source gratis',
+            description: 'Ikuti tes kepribadian Big Five open-source gratis. Belajar untuk mengetahui ciri-ciri kepribadian Anda dan bandingkan diri Anda dengan pasangan, kolega, teman atau keluarga Anda.'
+        },
+        title: 'Tes Kepribadian Lima Besar',
+        call_to_action: 'Ikuti tes gratis sekarang',
+        no_registration: '* Tidak perlu registrasi',
+        tests_taken: 'Lebih dari 700.000 orang telah mengikuti tes',
+        compare: {
+            title: 'Bandingkan diri Anda dengan orang lain',
+            text1: 'Bandingkan diri Anda dengan pasangan, kolega, teman, atau keluarga Anda.',
+            text2: 'Pelajari bagaimana Anda berbeda dari orang lain di lima domain:',
+            action: 'Lihat langsung beraksi'
+        },
+        share: 'Bagikan dengan teman-teman Anda dan cari tahu seberapa kompatibel Anda',
+        cards: {
+            open: {
+                title: 'Buka',
+                text: 'Ini adalah proyek open source di bawah lisensi MIT.'
+            },
+            free: {
+                title: 'Gratis',
+                text: 'Tes ini sepenuhnya gratis'
+            },
+            scientific: {
+                title: 'Ilmiah',
+                text: 'BigFive adalah model psikologis yang divalidasi secara ilmiah dan dapat diandalkan.'
+            },
+            translated: {
+                title: 'Diterjemahkan',
+                text: 'Diterjemahkan ke lebih dari 20 bahasa. Membantu <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank">sini</a>!'
+            }
+        },
+        description: {
+            top: 'Belajar mengenal diri Anda lebih baik dengan tes kepribadian sumber terbuka gratis.',
+            subtop: 'Itu',
+            subtop2: ' adalah model psikologis yang paling tervalidasi secara ilmiah dan dapat diandalkan untuk mengukur kepribadian.',
+            reference: `Tes dan evaluasi untuk situs ini dikumpulkan dari
+  <a href="http://ipip.ori.org" rel="noopener" target="_blank">ipip.ori.org</a>,
+  inventaris berasal<i>Johnson (2014) 120-item IPIP NEO-PI-R</i>.`,
+            info: 'Tes berikut berisi 120 soal yang diperkirakan membutuhkan waktu sekitar 10 menit untuk diselesaikan.',
+            result: 'Setelah Anda menyelesaikan tes, Anda akan menerima laporan kepribadian terperinci di domain berikut: ',
+            tests_taken: 'tes yang dilakukan sejauh ini'
+        }
+    },
+    about: {
+        seo: {
+            title: 'Tentang dan tim lima besar',
+            description: 'BigFive memiliki tim yang sangat aktif dan terlibat yang terus berusaha untuk mendorong BigFive ke depan.'
+        }
+    },
+    toolbar: {
+        home: 'Rumah',
+        result: 'Hasil',
+        compare: 'Membandingkan',
+        articles: 'Artikel',
+        privacy: 'Pribadi',
+        about: 'Tentang',
+        see_results: 'Lihat hasil Anda',
+        compare_with: 'Bandingkan dengan orang lain'
+    },
+    facets: {
+        openness_to_experience: {
+            title: 'Keterbukaan untuk mengalami'
+        },
+        conscientiousness: {
+            title: 'Kesadaran'
+        },
+        extraversion: {
+            title: 'Ekstraversi'
+        },
+        agreeableness: {
+            title: 'Persetujuan'
+        },
+        neuroticism: {
+            title: 'Neuroticism'
+        }
+    },
+    common: {
+        and: 'dan',
+        pages: 'halaman',
+        languages: 'bahasa'
+    },
+    form: {
+        information: 'Informasi',
+        informationText: 'Kami menggunakan informasi yang Anda berikan untuk melayani Anda',
+        mostAccurate: 'hasil yang paling akurat',
+        readMoreAbout: 'Baca lebih lanjut tentang',
+        privacy: 'pribadi',
+        nextButton: 'Oke, pertanyaan selanjutnya',
+        declineButton: 'lanjutkan tanpa berbagi',
+        prefferedLanguage: 'Bahasa pilihan saya adalah',
+        selectLanguage: 'Pilih bahasa',
+        iama: 'saya adalah seorang',
+        male: 'Pria',
+        female: 'Perempuan',
+        age: 'Usia',
+        iam: 'saya',
+        yearsOld: 'tahun',
+        ageWarning: 'Anda harus berusia 16 tahun ke atas',
+        language: 'Bahasa',
+        gender: 'Jenis kelamin',
+        confirmInfo: 'Konfirmasikan informasi',
+        confirm: 'Konfirmasi',
+        toTestButton: 'Baiklah, bawa saya ke tes'
+    },
+    test: {
+        next: 'lanjut',
+        back: 'kembali',
+        more: 'lebih',
+        seeResults: 'lihat hasilnya'
+    },
+    big_five: {
+        title: 'Apakah lima besar itu?',
+        seo: {
+            title: 'Apakah lima besar itu?',
+            description: 'Baca lebih lanjut tentang lima besar'
+        }
+    },
+    openness_to_experience: {
+        title: 'Keterbukaan terhadap Pengalaman',
+        seo: {
+            title: 'Gambaran umum tentang domain Openness To Experience.',
+            description: 'Baca lebih lanjut tentang domain Openness To Experience dalam model b5'
+        }
+    },
+    conscientiousness: {
+        title: 'Kesadaran',
+        seo: {
+            title: 'Sekilas tentang domain Conscientiousness',
+            description: 'Baca lebih lanjut tentang domain Conscientiousness dalam model b5'
+        }
+    },
+    extraversion: {
+        title: 'Ekstraversi',
+        seo: {
+            title: 'Tinjauan tentang ekstraversi domain',
+            description: 'Baca lebih lanjut tentang domain Extraversion dalam model b5'
+        }
+    },
+    agreeableness: {
+        title: 'Persetujuan',
+        seo: {
+            title: 'Ikhtisar domain Persetujuan',
+            description: 'Baca lebih lanjut tentang domain kesesuaian dalam model b5'
+        }
+    },
+    neuroticism: {
+        title: 'Neuroticism',
+        seo: {
+            title: 'Tinjauan tentang domain Neuroticism',
+            description: 'Baca lebih lanjut tentang domain Neuroticism dalam model b5'
+        }
+    },
+    getCompare: {
+        title: 'Bandingkan orang atau tim',
+        description1: 'Bandingkan hasil tes kepribadian bigfive dengan banyak orang.',
+        description2: 'Ketik ID yang Anda dapatkan dari hasil tes, mis.',
+        description3: 'di kolom ID-input',
+        needToAddPeople: 'Anda perlu menambahkan orang untuk membandingkan mereka',
+        addAnother: 'Tambahkan orang lain',
+        addPerson: 'Tambahkan Orang',
+        comparePeople: 'bandingkan orang',
+        nameOfPerson: 'Nama orang ini',
+        urlOrId: 'URL atau ID untuk perbandingan',
+        name: 'Nama'
+    },
+    results: {
+        theBigFive: 'Lima Besar',
+        score: 'skor',
+        important: 'Penting!',
+        saveResults: 'Simpan ID berikut untuk melihat hasilnya nanti atau',
+        compare: 'membandingkan',
+        toOthers: 'diri Anda kepada orang lain',
+        seo: {
+            title: 'Hasil saya dari tes kepribadian BigFive',
+            description: 'Lihat kepribadian saya dan bandingkan diri Anda dalam lima domain berikut: Neuroticism, Openness To Experience, Conscientiousness, Extraversion, Agreeableness'
+        }
+    },
+    getResult: {
+        result: 'Hasil',
+        explaination: 'Jika Anda telah mengikuti tes dan menyimpan ID Anda, Anda dapat melihat hasilnya di sini dengan mengetikkan ID yang Anda dapatkan, yaitu.',
+        idInput: 'di kolom ID-input',
+        urlOrId: 'URL atau ID hasil Anda',
+        getResult: 'Dapatkan hasil'
+    },
+    shareLinks: {
+        copyLink: 'Salin link ke clipboard',
+        shareFacebook: 'Bagikan di Facebook',
+        shareTwitter: 'Bagikan di Twitter',
+        copiedLink: 'Tautan disalin',
+        shareResults: 'Bagikan hasil Anda!'
+    },
+    opennessToExperience: {
+        quote: 'Openness to Experience menggambarkan dimensi gaya kognitif yang membedakan orang yang imajinatif dan kreatif dari orang yang sederhana dan konvensional.',
+        text1: `
+        <p>
+        Openness to Experience menggambarkan dimensi gaya kognitif yang membedakan orang yang imajinatif dan kreatif dari orang yang sederhana dan konvensional.
+        </p>
+        <p>
+        Orang-orang terbuka <b> ingin tahu secara intelektual, menghargai seni, dan peka terhadap keindahan </b>.
+        Mereka cenderung, dibandingkan dengan orang tertutup, lebih <b> menyadari perasaan mereka </b>.
+        Mereka cenderung berpikir dan bertindak dengan cara yang individualistis dan tidak sesuai.
+        </p>
+        <p>
+        Kaum intelektual biasanya mendapat skor tinggi pada Openness to Experience; akibatnya, faktor ini juga disebut Budaya atau Intelek.
+        </p>
+      `,
+        text2: `
+        <p>
+        Meskipun demikian, Akal mungkin paling baik dianggap sebagai salah satu aspek keterbukaan terhadap pengalaman.
+        Skor pada Keterbukaan terhadap Pengalaman hanya sedikit terkait dengan tahun pendidikan dan skor pada tes kecerdasan standar.
+        </p>
+        <p>
+        Karakteristik lain dari gaya kognitif terbuka adalah fasilitas untuk berpikir dalam simbol dan abstraksi yang jauh dari pengalaman konkret.
+        </p>
+        <p>
+        Bergantung pada kemampuan intelektual spesifik individu, kognisi simbolik ini dapat berbentuk pemikiran matematika, logis, atau geometris, penggunaan bahasa secara artistik dan metaforis, komposisi atau pertunjukan musik, atau salah satu dari banyak seni visual atau pertunjukan.
+        </p>
+        <div class="title">
+          Skor rendah
+        </div>
+        <p>
+        Orang dengan <b> skor rendah </b> pada keterbukaan terhadap pengalaman cenderung memiliki <b> minat yang sempit dan sama </b>.
+        </p>
+        <p>
+          Mereka lebih menyukai yang <b> polos, lugas </b>, dan jelas daripada yang rumit, ambigu, dan halus.
+        </p>
+        <p>
+        Mereka mungkin menganggap seni dan sains dengan curiga, menganggap upaya ini sebagai musykil atau tidak berguna secara praktis.
+        </p>
+        <p>
+        Orang tertutup lebih menyukai keakraban daripada hal baru; mereka konservatif dan tahan terhadap perubahan.
+        </p>
+        <p>
+        Keterbukaan sering kali ditampilkan sebagai lebih sehat atau lebih dewasa oleh psikolog, yang seringkali juga terbuka untuk pengalaman. Namun, gaya berpikir terbuka dan tertutup berguna dalam lingkungan yang berbeda.
+        </p>
+        <p>
+        Gaya intelektual orang terbuka mungkin dapat melayani seorang profesor dengan baik, tetapi penelitian telah menunjukkan bahwa pemikiran tertutup terkait dengan kinerja kerja yang unggul dalam pekerjaan polisi, penjualan, dan sejumlah pekerjaan layanan.
+        </p>
+  
+        <div class="title">
+        Imajinasi
+        </div>
+        <p>
+        Bagi individu yang imajinatif, dunia nyata seringkali terlalu sederhana dan biasa.
+        </p>
+        <p>
+        <b> Peraih skor tinggi </b> pada skala ini menggunakan fantasi sebagai cara untuk menciptakan dunia yang lebih kaya dan lebih menarik.
+        </p>
+        <p>
+        <b> Pencetak skor rendah </b> pada skala ini lebih berorientasi pada fakta daripada fantasi.
+        </p>
+  
+        <div class="title">
+        Minat Artistik
+        </div>
+        <p>
+        <b> Peraih skor tinggi </b> pada skala ini menyukai keindahan, baik dalam seni maupun alam. Mereka menjadi mudah terlibat dan terserap dalam peristiwa seni dan alam.
+        </p>
+        <p>
+        Mereka belum tentu terlatih secara artistik atau berbakat, meskipun banyak yang akan melakukannya. Ciri-ciri yang menentukan dari skala ini adalah ketertarikan, dan apresiasi terhadap keindahan alam dan buatan.
+        </p>
+        <p>
+        <b> Pencetak skor rendah </b> kurang memiliki kepekaan estetika dan minat pada seni.
+        </p>
+  
+        <div class="title">
+        Emosionalitas
+        </div>
+        <p>
+        Orang yang memiliki Emosionalitas tinggi memiliki akses dan kesadaran yang baik akan perasaan mereka sendiri.
+        </p>
+        <p>
+        Skor rendah kurang menyadari perasaan mereka dan cenderung tidak mengekspresikan emosi mereka secara terbuka.
+        </p>
+  
+        <div class="title">
+        Kepetualangan
+        </div>
+        <p>
+        Peraih skor tinggi dalam petualangan sangat ingin mencoba aktivitas baru, bepergian ke negeri asing, dan mengalami hal yang berbeda.
+        </p>
+        <p>
+        Mereka menemukan keakraban dan rutinitas yang membosankan, dan akan mengambil jalan baru pulang hanya karena itu berbeda.
+        </p>
+        <p>
+        Peraih skor rendah cenderung merasa tidak nyaman dengan perubahan dan lebih menyukai rutinitas yang sudah biasa.
+        </p>
+  
+        <div class="title">
+        Intelek
+        </div>
+        <p>
+          Kecerdasan dan minat artistik adalah dua aspek terpenting dari keterbukaan terhadap pengalaman. Peraih skor tinggi pada Intellect suka bermain-main dengan ide.
+        </p>
+        <p>
+        Mereka berpikiran terbuka terhadap ide-ide baru dan tidak biasa, dan suka memperdebatkan masalah intelektual. Mereka menyukai teka-teki, teka-teki, dan asah otak. Orang yang mendapat skor rendah pada Akal lebih suka berurusan dengan orang atau benda daripada ide.
+        </p>
+        <p>
+        Mereka menganggap latihan intelektual sebagai pemborosan waktu.
+        </p>
+        <p>
+        Akal tidak boleh disamakan dengan kecerdasan. Akal adalah gaya intelektual, bukan kemampuan intelektual, meskipun skor tinggi pada Kecerdasan mendapat skor sedikit lebih tinggi daripada individu dengan Kecerdasan rendah pada tes kecerdasan standar.
+        </p>
+  
+        <div class="title">
+        Liberalisme
+        </div>
+        <p>
+        Liberalisme psikologis mengacu pada kesiapan untuk menantang otoritas, konvensi, dan nilai-nilai tradisional.
+        </p>
+        <p>
+        Dalam bentuknya yang paling ekstrem, liberalisme psikologis bahkan dapat mewakili permusuhan langsung terhadap aturan, simpati bagi pelanggar hukum, dan cinta ambiguitas, kekacauan, dan kekacauan.
+        </p>
+        <p>
+        Kaum konservatif psikologis lebih menyukai keamanan dan stabilitas yang dibawa oleh kesesuaian dengan tradisi. Liberalisme psikologis dan konservatisme tidak identik dengan afiliasi politik, tetapi yang pasti mengarahkan individu ke partai politik tertentu.
+        </p>
+      `
+    }
+}
