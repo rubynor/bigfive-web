5cf79cc79fb1ceaeacadb22eb45b0c710859dfeb
Jonas Enge <jonas.enge@gmail.com>
2020-10-06 18:00:32 +0200

    Lint fix and add lang to sitemap

--- translations/fi.js
+++ translations/fi.js
@@ -1,205 +1,205 @@
 export default {
-    language: 'Finnish',
+  language: 'Finnish',
+  seo: {
+    keywords: 'iso viisi persoonallisuustesti, iso 5 persoonallisuustesti, b5-testi, bigfive-testi, persoonallisuuden piirteet, bigfive, vertaa, ilmainen, Jordan Peterson '
+  },
+  frontpage: {
     seo: {
-      keywords: 'iso viisi persoonallisuustesti, iso 5 persoonallisuustesti, b5-testi, bigfive-testi, persoonallisuuden piirteet, bigfive, vertaa, ilmainen, Jordan Peterson '
+      title: 'Ilmainen avoimen lähdekoodin BigFive-persoonallisuuspiirteiden testi',
+      description: 'Ota ilmainen, avoimen lähdekoodin Big Five -henkilöstötesti. Opi tuntemaan persoonallisuutesi ja vertaamaan itseäsi kumppaniin, kollegoihin, ystäviin tai perheeseen.'
     },
-    frontpage: {
-      seo: {
-        title: 'Ilmainen avoimen lähdekoodin BigFive-persoonallisuuspiirteiden testi',
-        description: 'Ota ilmainen, avoimen lähdekoodin Big Five -henkilöstötesti. Opi tuntemaan persoonallisuutesi ja vertaamaan itseäsi kumppaniin, kollegoihin, ystäviin tai perheeseen.'
+    title: 'Viiden suuren persoonallisuustesti',
+    call_to_action: 'Ota ilmainen testi nyt',
+    no_registration: '* Ei rekisteröintivaatimusta',
+    tests_taken: 'Testin on suorittanut yli 700 000 ihmistä',
+    compare: {
+      title: 'Vertaa itseäsi muihin',
+      text1: 'Vertaa itseäsi kumppaniin, kollegoihin, ystäviin tai perheeseen.',
+      text2: 'Opi, miten erotat muista viidestä verkkotunnuksesta:',
+      action: 'Näe se toiminnassa'
+    },
+    share: 'jaaYstävillesiJaSelvitä,kuinkaYhteensopivaOlet',
+    cards: {
+      open: {
+        title: 'Avata',
+        text: 'Tämä on avoimen lähdekoodin projekti MIT-lisenssillä.'
       },
-      title: 'Viiden suuren persoonallisuustesti',
-      call_to_action: 'Ota ilmainen testi nyt',
-      no_registration: '* Ei rekisteröintivaatimusta',
-      tests_taken: 'Testin on suorittanut yli 700 000 ihmistä',
-      compare: {
-        title: 'Vertaa itseäsi muihin',
-        text1: 'Vertaa itseäsi kumppaniin, kollegoihin, ystäviin tai perheeseen.',
-        text2: 'Opi, miten erotat muista viidestä verkkotunnuksesta:',
-        action: 'Näe se toiminnassa'
+      free: {
+        title: 'Vapaa',
+        text: 'Testi on täysin ilmainen'
       },
-      share: 'jaaYstävillesiJaSelvitä,kuinkaYhteensopivaOlet',
-      cards: {
-        open: {
-          title: 'Avata',
-          text: 'Tämä on avoimen lähdekoodin projekti MIT-lisenssillä.'
-        },
-        free: {
-          title: 'Vapaa',
-          text: 'Testi on täysin ilmainen'
-        },
-        scientific: {
-          title: 'Tieteellinen',
-          text: 'BigFive on tieteellisesti validoitu ja luotettava psykologinen malli.'
-        },
-        translated: {
-          title: 'Käännetty',
-          text: 'Käännetty yli 20 kielelle. Auttaa <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank">tässä</a>!'
-        }
+      scientific: {
+        title: 'Tieteellinen',
+        text: 'BigFive on tieteellisesti validoitu ja luotettava psykologinen malli.'
       },
-      description: {
-        top: 'Opi tuntemaan itsesi paremmin ilmaisen, avoimen lähdekoodin persoonallisuustestin avulla.',
-        subtop: 'The',
-        subtop2: ' on tieteellisesti validoitu ja luotettava psykologinen malli persoonallisuuden mittaamiseksi.',
-        reference: `Tämän sivuston testit ja arvioinnit on kerätty osoitteesta
+      translated: {
+        title: 'Käännetty',
+        text: 'Käännetty yli 20 kielelle. Auttaa <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank">tässä</a>!'
+      }
+    },
+    description: {
+      top: 'Opi tuntemaan itsesi paremmin ilmaisen, avoimen lähdekoodin persoonallisuustestin avulla.',
+      subtop: 'The',
+      subtop2: ' on tieteellisesti validoitu ja luotettava psykologinen malli persoonallisuuden mittaamiseksi.',
+      reference: `Tämän sivuston testit ja arvioinnit on kerätty osoitteesta
   <a href="http://ipip.ori.org" rel="noopener" target="_blank">ipip.ori.org</a>,
   varasto on peräisin <i>Johnsonin (2014) 120 nimikkeen IPIP NEO-PI-R</i>.`,
-        info: 'Seuraava testi sisältää 120 kysymystä, joiden suorittamisen arvioidaan vievän noin 10 minuuttia.',
-        result: 'Testin suorittamisen jälkeen saat yksityiskohtaisen persoonallisuusraportin seuraavista verkkotunnuksista:',
-        tests_taken: 'toistaiseksi tehdyt testit'
-      }
-    },
-    about: {
-      seo: {
-        title: 'Tietoja ja viisi suurta joukkuetta',
-        description: 'BigFivella on erittäin aktiivinen ja sitoutunut tiimi, joka pyrkii jatkuvasti työntämään BigFivea eteenpäin.'
-      }
-    },
-    toolbar: {
-      home: 'Koti',
-      result: 'Tulos',
-      compare: 'Vertailla',
-      articles: 'Artikkelit',
-      privacy: 'Yksityisyys',
-      about: 'Noin',
-      see_results: 'Katso tulokset',
-      compare_with: 'Vertaa muihin'
-    },
-    facets: {
-      openness_to_experience: {
-        title: 'Avoimuus kokemuksille'
-      },
-      conscientiousness: {
-        title: 'Tietoisuus'
-      },
-      extraversion: {
-        title: 'Extraversio'
-      },
-      agreeableness: {
-        title: 'Sopivuus'
-      },
-      neuroticism: {
-        title: 'Neurotiikka'
-      }
-    },
-    common: {
-      and: 'ja',
-      pages: 'sivuja',
-      languages: 'Kieli (kielet'
-    },
-    form: {
-      information: 'Tiedot',
-      informationText: 'Käytämme antamiasi tietoja palvelemaan sinua',
-      mostAccurate: 'mahdollisimman tarkka tulos',
-      readMoreAbout: 'Lue lisää',
-      privacy: 'yksityisyyttä',
-      nextButton: 'Okey, seuraava kysymys',
-      declineButton: 'jatka jakamatta',
-      prefferedLanguage: 'Haluttu kieleni on',
-      selectLanguage: 'Valitse kieli',
-      iama: 'minä olen',
-      male: 'Uros',
-      female: 'Nainen',
-      age: 'Ikä',
-      iam: 'olen',
-      yearsOld: 'vuotta vanha',
-      ageWarning: 'Sinun on oltava vähintään 16-vuotias',
-      language: 'Kieli',
-      gender: 'Sukupuoli',
-      confirmInfo: 'Vahvista tiedot',
-      confirm: 'Vahvistaa',
-      toTestButton: 'Selvä, vie minut testiin'
-    },
-    test: {
-      next: 'Seuraava',
-      back: 'takaisin',
-      more: 'lisää',
-      seeResults: 'katso tulokset'
-    },
-    big_five: {
-      title: 'Mitkä ovat viisi suurta?',
-      seo: {
-        title: 'Mitkä ovat viisi suurta?',
-        description: 'Lue lisää viidestä suuresta'
-      }
-    },
+      info: 'Seuraava testi sisältää 120 kysymystä, joiden suorittamisen arvioidaan vievän noin 10 minuuttia.',
+      result: 'Testin suorittamisen jälkeen saat yksityiskohtaisen persoonallisuusraportin seuraavista verkkotunnuksista:',
+      tests_taken: 'toistaiseksi tehdyt testit'
+    }
+  },
+  about: {
+    seo: {
+      title: 'Tietoja ja viisi suurta joukkuetta',
+      description: 'BigFivella on erittäin aktiivinen ja sitoutunut tiimi, joka pyrkii jatkuvasti työntämään BigFivea eteenpäin.'
+    }
+  },
+  toolbar: {
+    home: 'Koti',
+    result: 'Tulos',
+    compare: 'Vertailla',
+    articles: 'Artikkelit',
+    privacy: 'Yksityisyys',
+    about: 'Noin',
+    see_results: 'Katso tulokset',
+    compare_with: 'Vertaa muihin'
+  },
+  facets: {
     openness_to_experience: {
-      title: 'Avoimuus kokemukselle',
-      seo: {
-        title: 'Yleiskatsaus verkkotunnuksen avoimuudesta kokemukseen.',
-        description: 'Lue lisää Avoimuus kokea -verkkotunnuksesta b5-mallista'
-      }
+      title: 'Avoimuus kokemuksille'
     },
     conscientiousness: {
-      title: 'Tietoisuus',
-      seo: {
-        title: 'Yleiskatsaus verkkotunnuksesta Tunnollisuus',
-        description: 'Lue lisää omantunnon alasta b5-mallissa'
-      }
+      title: 'Tietoisuus'
     },
     extraversion: {
-      title: 'Extraversio',
-      seo: {
-        title: 'Yleiskatsaus verkkotunnuksen ekstraversiosta',
-        description: 'Lue lisää Extraversion-verkkotunnuksesta b5-mallista'
-      }
+      title: 'Extraversio'
     },
     agreeableness: {
-      title: 'Sopivuus',
-      seo: {
-        title: 'Yleiskatsaus verkkotunnuksen yhteensopivuuteen',
-        description: 'Lue lisää mukavuusalueesta b5-mallissa'
-      }
+      title: 'Sopivuus'
     },
     neuroticism: {
-      title: 'Neurotiikka',
-      seo: {
-        title: 'Yleiskatsaus verkkotunnuksen neurotismista',
-        description: 'Lue lisää neurotismialueesta b5-mallista'
-      }
-    },
-    getCompare: {
-      title: 'Vertaa ihmisiä tai joukkueita',
-      description1: 'Vertaa bigfive-persoonallisuustestin tuloksia useiden ihmisten kanssa.',
-      description2: 'Kirjoita testituloksista saamasi tunnus, ts.',
-      description3: 'ID-syöttökentässä',
-      needToAddPeople: 'Sinun on lisättävä ihmisiä vertaamaan niitä',
-      addAnother: 'Lisää toinen henkilö',
-      addPerson: 'Lisää henkilö',
-      comparePeople: 'vertaa ihmisiä',
-      nameOfPerson: 'Tämän henkilön nimi',
-      urlOrId: 'URL tai ID vertailua varten',
-      name: 'Nimi'
-    },
-    results: {
-      theBigFive: 'Iso viisi',
-      score: 'pisteet',
-      important: 'Tärkeä!',
-      saveResults: 'Tallenna seuraava tunnus nähdäksesi tulokset myöhemmin tai ',
-      compare: 'vertailla',
-      toOthers: 'itsesi muille',
-      seo: {
-        title: 'Tulokseni BigFive-persoonallisuustestistä',
-        description: 'Katso persoonallisuuttani ja vertaa itseäsi seuraavilla viidellä osa-alueella: neurotismi, avoimuus kokemukselle, tunnollisuus, ekstraversio, sopusointuisuus'
-      }
-    },
-    getResult: {
-      result: 'Tulos',
-      explaination: 'Jos olet suorittanut testin ja tallentanut henkilötodistuksesi, näet tulokset täältä kirjoittamalla saamasi henkilötodistuksen.',
-      idInput: 'ID-syöttökentässä',
-      urlOrId: 'Tuloksesi URL-osoite tai tunniste',
-      getResult: 'Hanki tulos'
-    },
-    shareLinks: {
-      copyLink: 'Kopioi linkki leikepöydälle',
-      shareFacebook: 'Jaa Facebookissa',
-      shareTwitter: 'Jaa Twitterissä',
-      copiedLink: 'Kopioitu linkki',
-      shareResults: 'Jaa tulokset!'
-    },
-    opennessToExperience: {
-      quote: 'Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.',
-      text1: `
+      title: 'Neurotiikka'
+    }
+  },
+  common: {
+    and: 'ja',
+    pages: 'sivuja',
+    languages: 'Kieli (kielet'
+  },
+  form: {
+    information: 'Tiedot',
+    informationText: 'Käytämme antamiasi tietoja palvelemaan sinua',
+    mostAccurate: 'mahdollisimman tarkka tulos',
+    readMoreAbout: 'Lue lisää',
+    privacy: 'yksityisyyttä',
+    nextButton: 'Okey, seuraava kysymys',
+    declineButton: 'jatka jakamatta',
+    prefferedLanguage: 'Haluttu kieleni on',
+    selectLanguage: 'Valitse kieli',
+    iama: 'minä olen',
+    male: 'Uros',
+    female: 'Nainen',
+    age: 'Ikä',
+    iam: 'olen',
+    yearsOld: 'vuotta vanha',
+    ageWarning: 'Sinun on oltava vähintään 16-vuotias',
+    language: 'Kieli',
+    gender: 'Sukupuoli',
+    confirmInfo: 'Vahvista tiedot',
+    confirm: 'Vahvistaa',
+    toTestButton: 'Selvä, vie minut testiin'
+  },
+  test: {
+    next: 'Seuraava',
+    back: 'takaisin',
+    more: 'lisää',
+    seeResults: 'katso tulokset'
+  },
+  big_five: {
+    title: 'Mitkä ovat viisi suurta?',
+    seo: {
+      title: 'Mitkä ovat viisi suurta?',
+      description: 'Lue lisää viidestä suuresta'
+    }
+  },
+  openness_to_experience: {
+    title: 'Avoimuus kokemukselle',
+    seo: {
+      title: 'Yleiskatsaus verkkotunnuksen avoimuudesta kokemukseen.',
+      description: 'Lue lisää Avoimuus kokea -verkkotunnuksesta b5-mallista'
+    }
+  },
+  conscientiousness: {
+    title: 'Tietoisuus',
+    seo: {
+      title: 'Yleiskatsaus verkkotunnuksesta Tunnollisuus',
+      description: 'Lue lisää omantunnon alasta b5-mallissa'
+    }
+  },
+  extraversion: {
+    title: 'Extraversio',
+    seo: {
+      title: 'Yleiskatsaus verkkotunnuksen ekstraversiosta',
+      description: 'Lue lisää Extraversion-verkkotunnuksesta b5-mallista'
+    }
+  },
+  agreeableness: {
+    title: 'Sopivuus',
+    seo: {
+      title: 'Yleiskatsaus verkkotunnuksen yhteensopivuuteen',
+      description: 'Lue lisää mukavuusalueesta b5-mallissa'
+    }
+  },
+  neuroticism: {
+    title: 'Neurotiikka',
+    seo: {
+      title: 'Yleiskatsaus verkkotunnuksen neurotismista',
+      description: 'Lue lisää neurotismialueesta b5-mallista'
+    }
+  },
+  getCompare: {
+    title: 'Vertaa ihmisiä tai joukkueita',
+    description1: 'Vertaa bigfive-persoonallisuustestin tuloksia useiden ihmisten kanssa.',
+    description2: 'Kirjoita testituloksista saamasi tunnus, ts.',
+    description3: 'ID-syöttökentässä',
+    needToAddPeople: 'Sinun on lisättävä ihmisiä vertaamaan niitä',
+    addAnother: 'Lisää toinen henkilö',
+    addPerson: 'Lisää henkilö',
+    comparePeople: 'vertaa ihmisiä',
+    nameOfPerson: 'Tämän henkilön nimi',
+    urlOrId: 'URL tai ID vertailua varten',
+    name: 'Nimi'
+  },
+  results: {
+    theBigFive: 'Iso viisi',
+    score: 'pisteet',
+    important: 'Tärkeä!',
+    saveResults: 'Tallenna seuraava tunnus nähdäksesi tulokset myöhemmin tai ',
+    compare: 'vertailla',
+    toOthers: 'itsesi muille',
+    seo: {
+      title: 'Tulokseni BigFive-persoonallisuustestistä',
+      description: 'Katso persoonallisuuttani ja vertaa itseäsi seuraavilla viidellä osa-alueella: neurotismi, avoimuus kokemukselle, tunnollisuus, ekstraversio, sopusointuisuus'
+    }
+  },
+  getResult: {
+    result: 'Tulos',
+    explaination: 'Jos olet suorittanut testin ja tallentanut henkilötodistuksesi, näet tulokset täältä kirjoittamalla saamasi henkilötodistuksen.',
+    idInput: 'ID-syöttökentässä',
+    urlOrId: 'Tuloksesi URL-osoite tai tunniste',
+    getResult: 'Hanki tulos'
+  },
+  shareLinks: {
+    copyLink: 'Kopioi linkki leikepöydälle',
+    shareFacebook: 'Jaa Facebookissa',
+    shareTwitter: 'Jaa Twitterissä',
+    copiedLink: 'Kopioitu linkki',
+    shareResults: 'Jaa tulokset!'
+  },
+  opennessToExperience: {
+    quote: 'Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.',
+    text1: `
         <p>
           Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.
         </p>
@@ -212,7 +212,7 @@ export default {
           Intellectuals typically score high on Openness to Experience; consequently, this factor has also been called Culture or Intellect.
         </p>
       `,
-      text2: `
+    text2: `
         <p>
           Nonetheless, Intellect is probably best regarded as one aspect of openness to experience.
           Scores on Openness to Experience are only modestly related to years of education and scores on standard intelligent tests.
@@ -323,6 +323,5 @@ export default {
           Psychological conservatives prefer the security and stability brought by conformity to tradition. Psychological liberalism and conservatism are not identical to political affiliation, but certainly incline individuals toward certain political parties.
         </p>
       `
-    }
   }
-  
+}
