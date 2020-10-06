76c695570fde4e9e3ec33d9c60a5053ac857fa08
JanitChawla <68466788+JanitChawla@users.noreply.github.com>
2020-10-06 21:07:22 +0530

    translated to finnish

---
+++ translations/fi.js
@@ -0,0 +1,328 @@
+export default {
+    language: 'Finish',
+    seo: {
+      keywords: 'iso viisi persoonallisuustesti, iso 5 persoonallisuustesti, b5-testi, bigfive-testi, persoonallisuuden piirteet, bigfive, vertaa, ilmainen, Jordan Peterson '
+    },
+    frontpage: {
+      seo: {
+        title: 'Ilmainen avoimen lähdekoodin BigFive-persoonallisuuspiirteiden testi',
+        description: 'Ota ilmainen, avoimen lähdekoodin Big Five -henkilöstötesti. Opi tuntemaan persoonallisuutesi ja vertaamaan itseäsi kumppaniin, kollegoihin, ystäviin tai perheeseen.'
+      },
+      title: 'Viiden suuren persoonallisuustesti',
+      call_to_action: 'Ota ilmainen testi nyt',
+      no_registration: '* Ei rekisteröintivaatimusta',
+      tests_taken: 'Testin on suorittanut yli 700 000 ihmistä',
+      compare: {
+        title: 'Vertaa itseäsi muihin',
+        text1: 'Vertaa itseäsi kumppaniin, kollegoihin, ystäviin tai perheeseen.',
+        text2: 'Opi, miten erotat muista viidestä verkkotunnuksesta:',
+        action: 'Näe se toiminnassa'
+      },
+      share: 'jaaYstävillesiJaSelvitä,kuinkaYhteensopivaOlet',
+      cards: {
+        open: {
+          title: 'Avata',
+          text: 'Tämä on avoimen lähdekoodin projekti MIT-lisenssillä.'
+        },
+        free: {
+          title: 'Vapaa',
+          text: 'Testi on täysin ilmainen'
+        },
+        scientific: {
+          title: 'Tieteellinen',
+          text: 'BigFive on tieteellisesti validoitu ja luotettava psykologinen malli.'
+        },
+        translated: {
+          title: 'Käännetty',
+          text: 'Käännetty yli 20 kielelle. Auttaa <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank">tässä</a>!'
+        }
+      },
+      description: {
+        top: 'Opi tuntemaan itsesi paremmin ilmaisen, avoimen lähdekoodin persoonallisuustestin avulla.',
+        subtop: 'The',
+        subtop2: ' on tieteellisesti validoitu ja luotettava psykologinen malli persoonallisuuden mittaamiseksi.',
+        reference: `Tämän sivuston testit ja arvioinnit on kerätty osoitteesta
+  <a href="http://ipip.ori.org" rel="noopener" target="_blank">ipip.ori.org</a>,
+  varasto on peräisin <i>Johnsonin (2014) 120 nimikkeen IPIP NEO-PI-R</i>.`,
+        info: 'Seuraava testi sisältää 120 kysymystä, joiden suorittamisen arvioidaan vievän noin 10 minuuttia.',
+        result: 'Testin suorittamisen jälkeen saat yksityiskohtaisen persoonallisuusraportin seuraavista verkkotunnuksista:',
+        tests_taken: 'toistaiseksi tehdyt testit'
+      }
+    },
+    about: {
+      seo: {
+        title: 'Tietoja ja viisi suurta joukkuetta',
+        description: 'BigFivella on erittäin aktiivinen ja sitoutunut tiimi, joka pyrkii jatkuvasti työntämään BigFivea eteenpäin.'
+      }
+    },
+    toolbar: {
+      home: 'Koti',
+      result: 'Tulos',
+      compare: 'Vertailla',
+      articles: 'Artikkelit',
+      privacy: 'Yksityisyys',
+      about: 'Noin',
+      see_results: 'Katso tulokset',
+      compare_with: 'Vertaa muihin'
+    },
+    facets: {
+      openness_to_experience: {
+        title: 'Avoimuus kokemuksille'
+      },
+      conscientiousness: {
+        title: 'Tietoisuus'
+      },
+      extraversion: {
+        title: 'Extraversio'
+      },
+      agreeableness: {
+        title: 'Sopivuus'
+      },
+      neuroticism: {
+        title: 'Neurotiikka'
+      }
+    },
+    common: {
+      and: 'ja',
+      pages: 'sivuja',
+      languages: 'Kieli (kielet'
+    },
+    form: {
+      information: 'Tiedot',
+      informationText: 'Käytämme antamiasi tietoja palvelemaan sinua',
+      mostAccurate: 'mahdollisimman tarkka tulos',
+      readMoreAbout: 'Lue lisää',
+      privacy: 'yksityisyyttä',
+      nextButton: 'Okey, seuraava kysymys',
+      declineButton: 'jatka jakamatta',
+      prefferedLanguage: 'Haluttu kieleni on',
+      selectLanguage: 'Valitse kieli',
+      iama: 'minä olen',
+      male: 'Uros',
+      female: 'Nainen',
+      age: 'Ikä',
+      iam: 'olen',
+      yearsOld: 'vuotta vanha',
+      ageWarning: 'Sinun on oltava vähintään 16-vuotias',
+      language: 'Kieli',
+      gender: 'Sukupuoli',
+      confirmInfo: 'Vahvista tiedot',
+      confirm: 'Vahvistaa',
+      toTestButton: 'Selvä, vie minut testiin'
+    },
+    test: {
+      next: 'Seuraava',
+      back: 'takaisin',
+      more: 'lisää',
+      seeResults: 'katso tulokset'
+    },
+    big_five: {
+      title: 'Mitkä ovat viisi suurta?',
+      seo: {
+        title: 'Mitkä ovat viisi suurta?',
+        description: 'Lue lisää viidestä suuresta'
+      }
+    },
+    openness_to_experience: {
+      title: 'Avoimuus kokemukselle',
+      seo: {
+        title: 'Yleiskatsaus verkkotunnuksen avoimuudesta kokemukseen.',
+        description: 'Lue lisää Avoimuus kokea -verkkotunnuksesta b5-mallista'
+      }
+    },
+    conscientiousness: {
+      title: 'Tietoisuus',
+      seo: {
+        title: 'Yleiskatsaus verkkotunnuksesta Tunnollisuus',
+        description: 'Lue lisää omantunnon alasta b5-mallissa'
+      }
+    },
+    extraversion: {
+      title: 'Extraversio',
+      seo: {
+        title: 'Yleiskatsaus verkkotunnuksen ekstraversiosta',
+        description: 'Lue lisää Extraversion-verkkotunnuksesta b5-mallista'
+      }
+    },
+    agreeableness: {
+      title: 'Sopivuus',
+      seo: {
+        title: 'Yleiskatsaus verkkotunnuksen yhteensopivuuteen',
+        description: 'Lue lisää mukavuusalueesta b5-mallissa'
+      }
+    },
+    neuroticism: {
+      title: 'Neurotiikka',
+      seo: {
+        title: 'Yleiskatsaus verkkotunnuksen neurotismista',
+        description: 'Lue lisää neurotismialueesta b5-mallista'
+      }
+    },
+    getCompare: {
+      title: 'Vertaa ihmisiä tai joukkueita',
+      description1: 'Vertaa bigfive-persoonallisuustestin tuloksia useiden ihmisten kanssa.',
+      description2: 'Kirjoita testituloksista saamasi tunnus, ts.',
+      description3: 'ID-syöttökentässä',
+      needToAddPeople: 'Sinun on lisättävä ihmisiä vertaamaan niitä',
+      addAnother: 'Lisää toinen henkilö',
+      addPerson: 'Lisää henkilö',
+      comparePeople: 'vertaa ihmisiä',
+      nameOfPerson: 'Tämän henkilön nimi',
+      urlOrId: 'URL tai ID vertailua varten',
+      name: 'Nimi'
+    },
+    results: {
+      theBigFive: 'Iso viisi',
+      score: 'pisteet',
+      important: 'Tärkeä!',
+      saveResults: 'Tallenna seuraava tunnus nähdäksesi tulokset myöhemmin tai ',
+      compare: 'vertailla',
+      toOthers: 'itsesi muille',
+      seo: {
+        title: 'Tulokseni BigFive-persoonallisuustestistä',
+        description: 'Katso persoonallisuuttani ja vertaa itseäsi seuraavilla viidellä osa-alueella: neurotismi, avoimuus kokemukselle, tunnollisuus, ekstraversio, sopusointuisuus'
+      }
+    },
+    getResult: {
+      result: 'Tulos',
+      explaination: 'Jos olet suorittanut testin ja tallentanut henkilötodistuksesi, näet tulokset täältä kirjoittamalla saamasi henkilötodistuksen.',
+      idInput: 'ID-syöttökentässä',
+      urlOrId: 'Tuloksesi URL-osoite tai tunniste',
+      getResult: 'Hanki tulos'
+    },
+    shareLinks: {
+      copyLink: 'Kopioi linkki leikepöydälle',
+      shareFacebook: 'Jaa Facebookissa',
+      shareTwitter: 'Jaa Twitterissä',
+      copiedLink: 'Kopioitu linkki',
+      shareResults: 'Jaa tulokset!'
+    },
+    opennessToExperience: {
+      quote: 'Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.',
+      text1: `
+        <p>
+          Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.
+        </p>
+        <p>
+          Open people are <b>intellectually curious, appreciative of art, and sensitive to beauty</b>.
+          They tend to be, compared to closed people, more <b>aware of their feelings</b>.
+          They tend to think and act in individualistic and nonconforming ways.
+        </p>
+        <p>
+          Intellectuals typically score high on Openness to Experience; consequently, this factor has also been called Culture or Intellect.
+        </p>
+      `,
+      text2: `
+        <p>
+          Nonetheless, Intellect is probably best regarded as one aspect of openness to experience.
+          Scores on Openness to Experience are only modestly related to years of education and scores on standard intelligent tests.
+        </p>
+        <p>
+          Another characteristic of the open cognitive style is a facility for thinking in symbols and abstractions far removed from concrete experience.
+        </p>
+        <p>
+          Depending on the individual's specific intellectual abilities, this symbolic cognition may take the form of mathematical, logical, or geometric thinking, artistic and metaphorical use of language, music composition or performance, or one of the many visual or performing arts.
+        </p>
+        <div class="title">
+          Low scores
+        </div>
+        <p>
+          People with <b>low scores</b> on openness to experience tend to have <b>narrow, common interests</b>.
+        </p>
+        <p>
+          They prefer the <b>plain, straightforward</b>, and obvious over the complex, ambiguous, and subtle.
+        </p>
+        <p>
+          They may regard the arts and sciences with suspicion, regarding these endeavors as abstruse or of no practical use.
+        </p>
+        <p>
+          Closed people prefer familiarity over novelty; they are conservative and resistant to change.
+        </p>
+        <p>
+          Openness is often presented as healthier or more mature by psychologists, who are often themselves open to experience. However, open and closed styles of thinking are useful in different environments.
+        </p>
+        <p>
+        The intellectual style of the open person may serve a professor well, but research has shown that closed thinking is related to superior job performance in police work, sales, and a number of service occupations.
+        </p>
+  
+        <div class="title">
+          Imagination
+        </div>
+        <p>
+          To imaginative individuals, the real world is often too plain and ordinary.
+        </p>
+        <p>
+          <b>High scorers</b> on this scale use fantasy as a way of creating a richer, more interesting world.
+        </p>
+        <p>
+        <b>Low scorers</b> are on this scale are more oriented to facts than fantasy.
+        </p>
+  
+        <div class="title">
+          Artistic Interests
+        </div>
+        <p>
+        <b>High scorers</b> on this scale love beauty, both in art and in nature. They become easily involved and absorbed in artistic and natural events.
+        </p>
+        <p>
+          They are not necessarily artistically trained nor talented, although many will be. The defining features of this scale are interest in, and appreciation of natural and artificial beauty.
+        </p>
+        <p>
+          <b>Low scorers</b> lack aesthetic sensitivity and interest in the arts.
+        </p>
+  
+        <div class="title">
+          Emotionality
+        </div>
+        <p>
+          Persons high on Emotionality have good access to and awareness of their own feelings.
+        </p>
+        <p>
+          Low scorers are less aware of their feelings and tend not to express their emotions openly.
+        </p>
+  
+        <div class="title">
+          Adventurousness
+        </div>
+        <p>
+          High scorers on adventurousness are eager to try new activities, travel to foreign lands, and experience different things.
+        </p>
+        <p>
+          They find familiarity and routine boring, and will take a new route home just because it is different.
+        </p>
+        <p>
+          Low scorers tend to feel uncomfortable with change and prefer familiar routines.
+        </p>
+  
+        <div class="title">
+          Intellect
+        </div>
+        <p>
+          Intellect and artistic interests are the two most important, central aspects of openness to experience. High scorers on Intellect love to play with ideas.
+        </p>
+        <p>
+          They are open-minded to new and unusual ideas, and like to debate intellectual issues. They enjoy riddles, puzzles, and brain teasers. Low scorers on Intellect prefer dealing with either people or things rather than ideas.
+        </p>
+        <p>
+          They regard intellectual exercises as a waste of time.
+        </p>
+        <p>
+          Intellect should not be equated with intelligence. Intellect is an intellectual style, not an intellectual ability, although high scorers on Intellect score slightly higher than low-Intellect individuals on standardized intelligence tests.
+        </p>
+  
+        <div class="title">
+          Liberalism
+        </div>
+        <p>
+          Psychological liberalism refers to a readiness to challenge authority, convention, and traditional values.
+        </p>
+        <p>
+          In its most extreme form, psychological liberalism can even represent outright hostility toward rules, sympathy for law-breakers, and love of ambiguity, chaos, and disorder.
+        </p>
+        <p>
+          Psychological conservatives prefer the security and stability brought by conformity to tradition. Psychological liberalism and conservatism are not identical to political affiliation, but certainly incline individuals toward certain political parties.
+        </p>
+      `
+    }
+  }
+  
