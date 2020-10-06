56003ff18c5e1a94973750e23d7ec0c7bb5d6a5c
Yash Walia <yashbest005@gmail.com>
2020-10-06 19:36:45 +0530

    run lintfix and add language to nuxt.config

--- translations/zh-Hans.js
+++ translations/zh-Hans.js
@@ -1,205 +1,205 @@
 export default {
-    language: 'Chinese',
+  language: 'Chinese',
+  seo: {
+    keywords: 'big five personality test, big 5 personality test, b5 test, bigfive test, personality traits, bigfive, compare, free, jordan peterson'
+  },
+  frontpage: {
     seo: {
-      keywords: 'big five personality test, big 5 personality test, b5 test, bigfive test, personality traits, bigfive, compare, free, jordan peterson'
+      title: 'Free open-source BigFive personality traits test',
+      description: 'Take a free, open-source Big Five personality test. Learn to know your personality traits and compare yourself with your partner, colleagues, friends or family.'
     },
-    frontpage: {
-      seo: {
-        title: 'Free open-source BigFive personality traits test',
-        description: 'Take a free, open-source Big Five personality test. Learn to know your personality traits and compare yourself with your partner, colleagues, friends or family.'
+    title: '五大人格测试',
+    call_to_action: '现在参加免费测试',
+    no_registration: '* 无需注册',
+    tests_taken: '超过700.000人参加了测试',
+    compare: {
+      title: '拿自己和别人比较',
+      text1: '拿自己和伴侣、同事、朋友或家人进行比较',
+      text2: '了解您与五个域中的其他人有何不同：',
+      action: '看到它在行动现场'
+    },
+    share: '与朋友分享，了解您的兼容性',
+    cards: {
+      open: {
+        title: '打开',
+        text: '这是 MIT 许可证下的开源项目'
       },
-      title: '五大人格测试',
-      call_to_action: '现在参加免费测试',
-      no_registration: '* 无需注册',
-      tests_taken: '超过700.000人参加了测试',
-      compare: {
-        title: '拿自己和别人比较',
-        text1: '拿自己和伴侣、同事、朋友或家人进行比较',
-        text2: '了解您与五个域中的其他人有何不同：',
-        action: '看到它在行动现场'
+      free: {
+        title: '免费',
+        text: '测试完全免费'
       },
-      share: '与朋友分享，了解您的兼容性',
-      cards: {
-        open: {
-          title: '打开',
-          text: '这是 MIT 许可证下的开源项目'
-        },
-        free: {
-          title: '免费',
-          text: '测试完全免费'
-        },
-        scientific: {
-          title: '科学',
-          text: 'BigFive 是一个经过科学验证且可靠的心理模型'
-        },
-        translated: {
-          title: '已翻译',
-          text: '翻译成 20 多种语言。帮助 <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank">here</a>!'
-        }
+      scientific: {
+        title: '科学',
+        text: 'BigFive 是一个经过科学验证且可靠的心理模型'
       },
-      description: {
-        top: '通过免费的开源个性测试，学会更好地了解自己。',
-        subtop: '的',
-        subtop2: ' 是衡量人格最科学、最可靠的心理模型。',
-        reference: `此站点的测试和评估来自
+      translated: {
+        title: '已翻译',
+        text: '翻译成 20 多种语言。帮助 <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank">here</a>!'
+      }
+    },
+    description: {
+      top: '通过免费的开源个性测试，学会更好地了解自己。',
+      subtop: '的',
+      subtop2: ' 是衡量人格最科学、最可靠的心理模型。',
+      reference: `此站点的测试和评估来自
   <a href="http://ipip.ori.org" rel="noopener" target="_blank">ipip.ori.org</a>,
   inventory is from <i>Johnson's (2014) 120-item IPIP NEO-PI-R</i>.`,
-        info: '以下测试包含 120 个问题，估计需要大约 10 分钟才能完成。',
-        result: 'After you have completed the test you will receive a detailed personality report in the following domains:',
-        tests_taken: 'tests taken so far'
-      }
-    },
-    about: {
-      seo: {
-        title: 'About and the big five team',
-        description: 'BigFive has a very active and engaged team that is constantly striving to push BigFive forward.'
-      }
-    },
-    toolbar: {
-      home: 'Home',
-      result: '结果',
-      compare: '比较',
-      articles: '文章',
-      privacy: '隐私',
-      about: '关于我们',
-      see_results: '查看您的结果',
-      compare_with: '与其他公司比较'
-    },
-    facets: {
-      openness_to_experience: {
-        title: '对经验的开放性'
-      },
-      conscientiousness: {
-        title: '责任心'
-      },
-      extraversion: {
-        title: '外向'
-      },
-      agreeableness: {
-        title: '同意'
-      },
-      neuroticism: {
-        title: '神经质'
-      }
-    },
-    common: {
-      and: '和',
-      pages: '页面',
-      languages: '语言'
-    },
-    form: {
-      information: 'Information',
-      informationText: 'We use the information you provide to serve you the',
-      mostAccurate: 'most accurate result possible',
-      readMoreAbout: 'Read more about',
-      privacy: 'privacy',
-      nextButton: 'Okey, next question',
-      declineButton: 'continue without sharing',
-      prefferedLanguage: '我最喜欢的语言是',
-      selectLanguage: '选择语言',
-      iama: 'I am a',
-      male: 'Male',
-      female: 'Female',
-      age: 'Age',
-      iam: 'I am',
-      yearsOld: 'years old',
-      ageWarning: 'You must be 16 or older',
-      language: '语言',
-      gender: 'Gender',
-      confirmInfo: 'Confirm information',
-      confirm: 'Confirm',
-      toTestButton: 'Alrighty, take me to the test'
-    },
-    test: {
-      next: '下一个',
-      back: '返回',
-      more: '更',
-      seeResults: '查看结果'
-    },
-    big_five: {
-      title: 'What are the big five?',
-      seo: {
-        title: 'What are the big five?',
-        description: 'Read more about the big five'
-      }
-    },
+      info: '以下测试包含 120 个问题，估计需要大约 10 分钟才能完成。',
+      result: 'After you have completed the test you will receive a detailed personality report in the following domains:',
+      tests_taken: 'tests taken so far'
+    }
+  },
+  about: {
+    seo: {
+      title: 'About and the big five team',
+      description: 'BigFive has a very active and engaged team that is constantly striving to push BigFive forward.'
+    }
+  },
+  toolbar: {
+    home: 'Home',
+    result: '结果',
+    compare: '比较',
+    articles: '文章',
+    privacy: '隐私',
+    about: '关于我们',
+    see_results: '查看您的结果',
+    compare_with: '与其他公司比较'
+  },
+  facets: {
     openness_to_experience: {
-      title: '开放体验',
-      seo: {
-        title: 'Overview of the domain Openness To Experience.',
-        description: 'Read more about the Openness To Experience domain in the b5 model'
-      }
+      title: '对经验的开放性'
     },
     conscientiousness: {
-      title: '责任心',
-      seo: {
-        title: 'Overview of the domain Conscientiousness',
-        description: 'Read more about the Conscientiousness domain in the b5 model'
-      }
+      title: '责任心'
     },
     extraversion: {
-      title: '外向',
-      seo: {
-        title: 'Overview of the domain Extraversion',
-        description: 'Read more about the Extraversion domain in the b5 model'
-      }
+      title: '外向'
     },
     agreeableness: {
-      title: '同意',
-      seo: {
-        title: 'Overview of the domain Agreeableness',
-        description: 'Read more about the agreeableness domain in the b5 model'
-      }
+      title: '同意'
     },
     neuroticism: {
-      title: '神经质',
-      seo: {
-        title: 'Overview of the domain Neuroticism',
-        description: 'Read more about the Neuroticism domain in the b5 model'
-      }
-    },
-    getCompare: {
-      title: '比较人员或团队',
-      description1: '将五大人格测试的结果与多人进行比较。',
-      description2: '输入您从测试结果（即',
-      description3: '在 ID 输入字段中',
-      needToAddPeople: '您需要添加人员来比较他们',
-      addAnother: '添加其他人',
-      addPerson: '添加人员',
-      comparePeople: '比较人',
-      nameOfPerson: '此人的姓名',
-      urlOrId: '用于比较的 URL 或 ID',
-      name: '姓名'
-    },
-    results: {
-      theBigFive: '五大',
-      score: '得分',
-      important: '重要！',
-      saveResults: '保存以下 ID 以查看以后的结果或',
-      compare: '比较',
-      toOthers: '自己对别人',
-      seo: {
-        title: 'My results from the BigFive personality test',
-        description: 'See my personality and compare yourself in the following five domains: Neuroticism, Openness To Experience, Conscientiousness, Extraversion, Agreeableness'
-      }
-    },
-    getResult: {
-      result: '结果',
-      explaination: '如果您参加了测试并保存了 ID，则通过键入您得到的 ID（即',
-      idInput: '在 ID 输入字段中',
-      urlOrId: '结果的 URL 或 ID',
-      getResult: '获取结果'
-    },
-    shareLinks: {
-      copyLink: '将链接复制到剪贴板',
-      shareFacebook: '在 Facebook 上分享',
-      shareTwitter: '在推特上分享',
-      copiedLink: '复制链接',
-      shareResults: '分享您的结果！'
-    },
-    opennessToExperience: {
-      quote: 'Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.',
-      text1: `
+      title: '神经质'
+    }
+  },
+  common: {
+    and: '和',
+    pages: '页面',
+    languages: '语言'
+  },
+  form: {
+    information: 'Information',
+    informationText: 'We use the information you provide to serve you the',
+    mostAccurate: 'most accurate result possible',
+    readMoreAbout: 'Read more about',
+    privacy: 'privacy',
+    nextButton: 'Okey, next question',
+    declineButton: 'continue without sharing',
+    prefferedLanguage: '我最喜欢的语言是',
+    selectLanguage: '选择语言',
+    iama: 'I am a',
+    male: 'Male',
+    female: 'Female',
+    age: 'Age',
+    iam: 'I am',
+    yearsOld: 'years old',
+    ageWarning: 'You must be 16 or older',
+    language: '语言',
+    gender: 'Gender',
+    confirmInfo: 'Confirm information',
+    confirm: 'Confirm',
+    toTestButton: 'Alrighty, take me to the test'
+  },
+  test: {
+    next: '下一个',
+    back: '返回',
+    more: '更',
+    seeResults: '查看结果'
+  },
+  big_five: {
+    title: 'What are the big five?',
+    seo: {
+      title: 'What are the big five?',
+      description: 'Read more about the big five'
+    }
+  },
+  openness_to_experience: {
+    title: '开放体验',
+    seo: {
+      title: 'Overview of the domain Openness To Experience.',
+      description: 'Read more about the Openness To Experience domain in the b5 model'
+    }
+  },
+  conscientiousness: {
+    title: '责任心',
+    seo: {
+      title: 'Overview of the domain Conscientiousness',
+      description: 'Read more about the Conscientiousness domain in the b5 model'
+    }
+  },
+  extraversion: {
+    title: '外向',
+    seo: {
+      title: 'Overview of the domain Extraversion',
+      description: 'Read more about the Extraversion domain in the b5 model'
+    }
+  },
+  agreeableness: {
+    title: '同意',
+    seo: {
+      title: 'Overview of the domain Agreeableness',
+      description: 'Read more about the agreeableness domain in the b5 model'
+    }
+  },
+  neuroticism: {
+    title: '神经质',
+    seo: {
+      title: 'Overview of the domain Neuroticism',
+      description: 'Read more about the Neuroticism domain in the b5 model'
+    }
+  },
+  getCompare: {
+    title: '比较人员或团队',
+    description1: '将五大人格测试的结果与多人进行比较。',
+    description2: '输入您从测试结果（即',
+    description3: '在 ID 输入字段中',
+    needToAddPeople: '您需要添加人员来比较他们',
+    addAnother: '添加其他人',
+    addPerson: '添加人员',
+    comparePeople: '比较人',
+    nameOfPerson: '此人的姓名',
+    urlOrId: '用于比较的 URL 或 ID',
+    name: '姓名'
+  },
+  results: {
+    theBigFive: '五大',
+    score: '得分',
+    important: '重要！',
+    saveResults: '保存以下 ID 以查看以后的结果或',
+    compare: '比较',
+    toOthers: '自己对别人',
+    seo: {
+      title: 'My results from the BigFive personality test',
+      description: 'See my personality and compare yourself in the following five domains: Neuroticism, Openness To Experience, Conscientiousness, Extraversion, Agreeableness'
+    }
+  },
+  getResult: {
+    result: '结果',
+    explaination: '如果您参加了测试并保存了 ID，则通过键入您得到的 ID（即',
+    idInput: '在 ID 输入字段中',
+    urlOrId: '结果的 URL 或 ID',
+    getResult: '获取结果'
+  },
+  shareLinks: {
+    copyLink: '将链接复制到剪贴板',
+    shareFacebook: '在 Facebook 上分享',
+    shareTwitter: '在推特上分享',
+    copiedLink: '复制链接',
+    shareResults: '分享您的结果！'
+  },
+  opennessToExperience: {
+    quote: 'Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.',
+    text1: `
         <p>
           Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.
         </p>
@@ -212,7 +212,7 @@ export default {
           Intellectuals typically score high on Openness to Experience; consequently, this factor has also been called Culture or Intellect.
         </p>
       `,
-      text2: `
+    text2: `
         <p>
           Nonetheless, Intellect is probably best regarded as one aspect of openness to experience.
           Scores on Openness to Experience are only modestly related to years of education and scores on standard intelligent tests.
@@ -323,6 +323,5 @@ export default {
           Psychological conservatives prefer the security and stability brought by conformity to tradition. Psychological liberalism and conservatism are not identical to political affiliation, but certainly incline individuals toward certain political parties.
         </p>
       `
-    }
   }
-  
+}
