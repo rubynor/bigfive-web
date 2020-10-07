25fcbc3c0b3db82b9ffc8b61eec68ba528757d87
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2020-10-07 17:42:35 -0300

    Add pt-br translation

---
+++ translations/pt-BR.js
@@ -0,0 +1,327 @@
+export default {
+  language: 'Brazilian Portuguese',
+  seo: {
+    keywords: 'teste de personalidade big five, teste de personalidade big 5, teste b5, teste bigfive, traços de personalidade, bigfive, comparar, gratuito, jordan peterson'
+  },
+  frontpage: {
+    seo: {
+      title: 'Teste BigFive de traços de personalidade gratuito e de código aberto',
+      description: 'Faça um teste de personalidade BigFive de código aberto gratuito. Aprenda a conhecer seus traços de personalidade e compare-se com seu parceiro ou parceira, colegas, amigos ou família.'
+    },
+    title: 'Teste de Personalidade Big Five',
+    call_to_action: 'Faça um teste gratuito agora',
+    no_registration: '* Não precisa se registrar',
+    tests_taken: 'Mais de 700.000 pessoas fizeram o teste',
+    compare: {
+      title: 'Compare-se com outros',
+      text1: 'Compare-se com o seu parceiro ou parceira, colegas, amigos ou família.',
+      text2: 'Saiba como você difere de outras pessoas em cinco domínios:',
+      action: 'Veja ao vivo em ação'
+    },
+    share: 'Compartilhe com seus amigos e descubra o quão compatíveis vocês são',
+    cards: {
+      open: {
+        title: 'Aberto',
+        text: 'Este é um projeto de código aberto sob a licença MIT.'
+      },
+      free: {
+        title: 'Gratuito',
+        text: 'O teste é totalmente gratuito'
+      },
+      scientific: {
+        title: 'Científico',
+        text: 'BigFive é um modelo psicológico cientificamente validado e confiável.'
+      },
+      translated: {
+        title: 'Traduzido',
+        text: 'Traduzido para mais de 20 idiomas. Ajude <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank">aqui</a>!'
+      }
+    },
+    description: {
+      top: 'Aprenda a se conhecer melhor com um teste de personalidade gratuito e de código aberto.',
+      subtop: 'O',
+      subtop2: ' é o modelo psicológico mais validado cientificamente e confiável para medir a personalidade.',
+      reference: `Os testes e a avaliação deste site são coletados de
+<a href="http://ipip.ori.org" rel="noopener" target="_blank">ipip.ori.org</a>,
+inventário é de <i>Johnson's (2014) 120-item IPIP NEO-PI-R</i>.`,
+      info: 'O teste a seguir contém 120 questões que levam cerca de 10 minutos para serem concluídas.',
+      result: 'Depois de concluir o teste, você receberá um relatório de personalidade detalhado nos seguintes domínios:',
+      tests_taken: 'testes feitos até agora'
+    }
+  },
+  about: {
+    seo: {
+      title: 'Sobre a equipe big five',
+      description: 'BigFive tem uma equipe muito ativa e engajada que está constantemente se esforçando para levar o BigFive adiante.'
+    }
+  },
+  toolbar: {
+    home: 'Início',
+    result: 'Resultado',
+    compare: 'Compare',
+    articles: 'Artigos',
+    privacy: 'Privcidade',
+    about: 'Sobre',
+    see_results: 'Veja seus resultados',
+    compare_with: 'Compare com outros'
+  },
+  facets: {
+    openness_to_experience: {
+      title: 'Abertura à experiência'
+    },
+    conscientiousness: {
+      title: 'Conscienciosidade'
+    },
+    extraversion: {
+      title: 'Extroversão'
+    },
+    agreeableness: {
+      title: 'Amabilidade'
+    },
+    neuroticism: {
+      title: 'Neuroticismo'
+    }
+  },
+  common: {
+    and: 'e',
+    pages: 'páginas',
+    languages: 'idiomas'
+  },
+  form: {
+    information: 'Informação',
+    informationText: 'Usamos as informações que você fornece para proporcionar o',
+    mostAccurate: 'resultado mais preciso possível',
+    readMoreAbout: 'Leia mais sobre',
+    privacy: 'privacidade',
+    nextButton: 'Ok, próxima pergunta',
+    declineButton: 'continue sem compartilhar',
+    prefferedLanguage: 'Meu idioma preferido é',
+    selectLanguage: 'Selecione um idioma',
+    iama: 'Eu sou',
+    male: 'Homem',
+    female: 'Mulher',
+    age: 'Idade',
+    iam: 'Eu sou',
+    yearsOld: 'anos de idade',
+    ageWarning: 'Você deve ter 16 anos ou mais',
+    language: 'Idioma',
+    gender: 'exo',
+    confirmInfo: 'Confirme a informação',
+    confirm: 'Confirme',
+    toTestButton: 'Tudo certo, leve-me para o teste'
+  },
+  test: {
+    next: 'avançar',
+    back: 'voltar',
+    more: 'mais',
+    seeResults: 'ver resultados'
+  },
+  big_five: {
+    title: 'Quais são os cinco grandes?',
+    seo: {
+      title: 'Quais são os cinco grandes?',
+      description: 'Leia mais sobre os cinco grandes'
+    }
+  },
+  openness_to_experience: {
+    title: 'Abertura à experiência',
+    seo: {
+      title: 'Visão geral do domínio Abertura à Experiência.',
+      description: 'Leia mais sobre o domínio Abertura à Experiência no modelo b5'
+    }
+  },
+  conscientiousness: {
+    title: 'Conscienciosidade',
+    seo: {
+      title: 'Visão geral do domínio Conscienciosidade',
+      description: 'Leia mais sobre o domínio Conscienciosidade no modelo b5'
+    }
+  },
+  extraversion: {
+    title: 'Extroversão',
+    seo: {
+      title: 'Visão geral do domínio Extroversão',
+      description: 'Leia mais sobre o domínio Extroversão no modelo b5'
+    }
+  },
+  agreeableness: {
+    title: 'Amabilidade',
+    seo: {
+      title: 'Visão geral do domínio Amabilidade',
+      description: 'Leia mais sobre o domínio amabilidade no modelo b5'
+    }
+  },
+  neuroticism: {
+    title: 'Neuroticismo',
+    seo: {
+      title: 'Visão geral do domínio Neuroticismo',
+      description: 'Leia mais sobre o domínio Neuroticismo no modelo b5'
+    }
+  },
+  getCompare: {
+    title: 'Compare pessoas ou equipes',
+    description1: 'Compare os resultados dotestes de personalidade bigfive com várias pessoas.',
+    description2: 'Digite a ID que você obteve dos resultados do teste, ou seja,',
+    description3: 'no campo ID',
+    needToAddPeople: 'Você precisa adicionar pessoas para compará-las',
+    addAnother: 'Adicionar outra pessoa',
+    addPerson: 'Adicionar pessoa',
+    comparePeople: 'comparar pessoas',
+    nameOfPerson: 'Nome desta pessoa',
+    urlOrId: 'URL ou ID para comparação',
+    name: 'Nome'
+  },
+  results: {
+    theBigFive: 'Os Cinco Grandes',
+    score: 'pontuação',
+    important: 'Importante!',
+    saveResults: 'Salve a seguinte ID para ver os resultados depois ou ',
+    compare: 'compare-se',
+    toOthers: 'com ou outros',
+    seo: {
+      title: 'Meus resultados do teste de personalidade BigFive',
+      description: 'Veja minha personalidade e compare-se nos cinco domínios seguintes: Neuroticismo, Abertura à Experiência, Consciência, Extroversão, Amabilidade'
+    }
+  },
+  getResult: {
+    result: 'Resultado',
+    explaination: 'Se você fez o teste e salvou sua ID, pode ver os resultados aqui digitando a ID que obteve, ou seja, ',
+    idInput: 'no campo ID',
+    urlOrId: 'URL ou ID do seu resultado',
+    getResult: 'Obter resultado'
+  },
+  shareLinks: {
+    copyLink: 'Copiar link para a área de transferência',
+    shareFacebook: 'Compartilhar no Facebook',
+    shareTwitter: 'Compartilhar no Twitter',
+    copiedLink: 'Link copiado',
+    shareResults: 'Compartilhe seus resultados!'
+  },
+  opennessToExperience: {
+    quote: 'A abertura à experiência descreve uma dimensão de estilo cognitivo que distingue pessoas imaginativas e criativas de pessoas realistas e convencionais.',
+    text1: `
+      <p>
+        A abertura à experiência descreve uma dimensão de estilo cognitivo que distingue pessoas imaginativas e criativas de pessoas realistas e convencionais.
+      </p>
+      <p>
+        Pessoas abertas são <b>curiosas intelectualmente, apreciam a arte e são sensíveis à beleza</b>.
+        Eles tendem a ser, em comparação com pessoas fechadas, mais <b>conscientes de seus sentimentos</b>.
+        Eles tendem a pensar e agir de maneiras individualistas e inconformadas.
+      </p>
+      <p>
+        Os intelectuais geralmente pontuam alto em Abertura à Experiência; consequentemente, esse fator também foi denominado Cultura ou Intelecto.
+      </p>
+    `,
+    text2: `
+      <p>
+        No entanto, o Intelecto é provavelmente melhor considerado como um aspecto da abertura à experiência.
+        As pontuações em Abertura à Experiência são apenas modestamente relacionadas aos anos de educação e às pontuações em testes de inteligência padrão.
+      </p>
+      <p>
+        Outra característica do estilo cognitivo aberto é a facilidade de pensar em símbolos e abstrações muito distantes da experiência concreta.
+      </p>
+      <p>
+        Dependendo das habilidades intelectuais específicas do indivíduo, esta cognição simbólica pode assumir a forma de pensamento matemático, lógico ou geométrico, uso artístico e metafórico da linguagem, composição musical ou performance, ou uma das muitas artes visuais ou performáticas.
+      </p>
+      <div class="title">
+        Pontuações baixas
+      </div>
+      <p>
+        Pessoas com <b>pontuação baixa</b> em abertura à experiência tendem a ter <b>interesses estreitos e comuns</b>.
+      </p>
+      <p>
+        Elas preferem o <b>simples, direto</b> e óbvio mais que o complexo, ambíguo e sutil.
+      </p>
+      <p>
+        Eles podem considerar as artes e ciências com suspeita, considerando esses esforços como obscuros ou sem uso prático.
+      </p>
+      <p>
+        Pessoas fechadas preferem familiaridade mais que novidade; elas são conservadores e resistentes a mudanças.
+      </p>
+      <p>
+        A abertura é frequentemente apresentada como mais saudável ou mais madura pelos psicólogos, que são muitas vezes eles mesmos abertos à experiência. No entanto, estilos de pensamento abertos e fechados são úteis em ambientes diferentes.
+      </p>
+      <p>
+        O estilo intelectual da pessoa aberta pode servir bem a um professor, mas a pesquisa mostrou que o pensamento fechado está relacionado ao desempenho superior no trabalho policial, vendas e uma série de ocupações de serviços.
+      </p>
+
+      <div class="title">
+        Imaginação
+      </div>
+      <p>
+        Para indivíduos imaginativos, o mundo real costuma ser muito simples e comum.
+      </p>
+      <p>
+        <b>Pessoas com pontuação alta</b> nesta escala usam a fantasia como uma forma de criar um mundo mais rico e interessante.
+      </p>
+      <p>
+        <b>Pessoas com pontuação baixa</b> estão nesta escala são mais orientadas para os fatos do que para a fantasia.
+      </p>
+
+      <div class="title">
+        Interesses Artísticos
+      </div>
+      <p>
+        <b>Os maiores pontuadores</b> nesta escala amam a beleza, tanto na arte quanto na natureza. Eles se tornam facilmente envolvidos e absorvidos em eventos artísticos e naturais.
+      </p>
+      <p>
+        Eles não são necessariamente treinados artisticamente nem talentosos, embora muitos sejam. As características definidoras desta escala são o interesse e a apreciação da beleza natural e artificial.
+      </p>
+      <p>
+        <b>Pessoas com pontuação baixa</b> carecem de sensibilidade estética e interesse nas artes.
+      </p>
+
+      <div class="title">
+        Emotividade
+      </div>
+      <p>
+        Pessoas com pontuação alta em Emotividade têm bom acesso e consciência de seus próprios sentimentos.
+      </p>
+      <p>
+        Pessoas com pontuação baixa são menos conscientes de seus sentimentos e tendem a não expressar suas emoções abertamente.
+      </p>
+
+      <div class="title">
+        Audácia
+      </div>
+      <p>
+        Pessoas com pontuação alta em audácia estão ansiosas para experimentar novas atividades, viajar para terras estrangeiras e experimentar coisas diferentes.
+      </p>
+      <p>
+        Eles acham a familiaridade e a rotina chatas, e farão um novo caminho para casa só porque é diferente.
+      </p>
+      <p>
+        Pessoas com pontuação baixa tendem a se sentir desconfortáveis com as mudanças e preferem rotinas conhecidas.
+      </p>
+
+      <div class="title">
+        Intelecto
+      </div>
+      <p>
+        Intelecto e interesses artísticos são os dois aspectos centrais mais importantes da abertura à experiência. Pessoas com pontuação alta em Intelecto adoram brincar com ideias.
+      </p>
+      <p>
+        Eles têm a mente aberta a ideias novas e incomuns e gostam de debater questões intelectuais. Eles gostam de enigmas, jogos e quebra-cabeças. Indivíduos com pontuação baixa em Intelecto preferem lidar com pessoas ou coisas em vez de ideias.
+      </p>
+      <p>
+        Eles consideram exercícios intelectuais uma perda de tempo.
+      </p>
+      <p>
+        Intelecto não deve ser equiparado à inteligência. Intelecto é um estilo intelectual, não uma habilidade intelectual, embora pessoas com pontuação alta no Intelecto pontuem um pouco mais do que indivíduos com baixo Intelecto em testes de inteligência padronizados.
+      </p>
+
+      <div class="title">
+        Liberalismo
+      </div>
+      <p>
+        O liberalismo psicológico se refere à prontidão para desafiar a autoridade, as convenções e os valores tradicionais.
+      </p>
+      <p>
+        Em sua forma mais extrema, o liberalismo psicológico pode até representar uma hostilidade absoluta em relação às regras, simpatia pelos transgressores da lei e amor à ambiguidade, ao caos e à desordem.
+      </p>
+      <p>
+        Os conservadores psicológicos preferem a segurança e a estabilidade trazidas pela conformidade com a tradição. O liberalismo psicológico e o conservadorismo não são idênticos à filiação política, mas certamente inclinam os indivíduos para certos partidos políticos.
+      </p>
+    `
+  }
+}
