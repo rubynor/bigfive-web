aec4391e5940da85d83e0ac233d144b8cc02c8bf
Harshal S Zodape <61976596+Harshalszz@users.noreply.github.com>
2020-10-07 19:42:52 +0530

    Added Japanese Translation

---
+++ translations/ja.js
@@ -0,0 +1,328 @@
+export default {
+    language: 'Japanese',
+    seo: {
+      keywords: 'ビッグファイブパーソナリティテスト、ビッグ5パーソナリティテスト、b5テスト、ビッグファイブテスト、パーソナリティ特性、ビッグファイブ、比較、無料、ジョーダンピーターソン'
+    },
+    frontpage: {
+      seo: {
+        title: '無料のオープンソースBigFive性格特性テスト',
+        description: '無料のオープンソースのビッグファイブパーソナリティテストを受けてください。あなたの性格特性を知り、あなた自身をあなたのパートナー、同僚、友人または家族と比較することを学びましょう。'
+      },
+      title: 'ビッグファイブパーソナリティテスト',
+      call_to_action: '今すぐ無料テストを受ける',
+      no_registration: '*登録は必要ありません',
+      tests_taken: '700.000人以上がテストを受けました',
+      compare: {
+        title: '自分を他の人と比較する',
+        text1: 'あなたのパートナー、同僚、友人または家族とあなた自身を比較してください。',
+        text2: '5つのドメインで他の人とどのように違うかを学びましょう。',
+        action: '実際に動作するのを見る'
+      },
+      share: '友達と共有して、あなたがどれだけ互換性があるかを調べてください',
+      cards: {
+        open: {
+          title: '開いた',
+          text: 'これはMITライセンスの下でのオープンソースプロジェクトです。'
+        },
+        free: {
+          title: '自由',
+          text: 'テストは完全に無料です'
+        },
+        scientific: {
+          title: '科学的',
+          text: 'BigFiveは、科学的に検証された信頼できる心理モデルです。'
+        },
+        translated: {
+          title: '翻訳済み',
+          text: '20以上の言語に翻訳されています。 <a href="https://b5.translations.alheimsins.net/" rel="noreferrer"target="blank">こちら</a>をご覧ください。'
+        }
+      },
+      description: {
+        top: '無料のオープンソースの性格検査で自分自身をよりよく知ることを学びましょう。',
+        subtop: 'ザ・',
+        subtop2: ' は、人格を測定するための最も科学的に検証された信頼できる心理モデルです。',
+        reference: `Tests and evaluation for this site is gathered from
+  <a href="http://ipip.ori.org" rel="noopener" target="_blank">ipip.ori.org</a>,
+  inventory is from <i>Johnson's (2014) 120-item IPIP NEO-PI-R</i>.`,
+        info: '次のテストには120の質問が含まれており、完了するまでに約10分かかると推定されています。',
+        result: 'テストが完了すると、次のドメインで詳細なパーソナリティレポートが届きます。',
+        tests_taken: 'これまでに行われたテスト'
+      }
+    },
+    about: {
+      seo: {
+        title: 'アバウトとビッグファイブチーム',
+        description: 'BigFiveには非常に活発で熱心なチームがあり、BigFiveを前進させるために絶えず努力しています。'
+      }
+    },
+    toolbar: {
+      home: 'ホーム',
+      result: '結果',
+      compare: '比較する',
+      articles: '記事',
+      privacy: 'プライバシー',
+      about: '約',
+      see_results: '結果を見る',
+      compare_with: '他の人と比較する'
+    },
+    facets: {
+      openness_to_experience: {
+        title: '経験への開放性'
+      },
+      conscientiousness: {
+        title: '誠実性'
+      },
+      extraversion: {
+        title: '外向性'
+      },
+      agreeableness: {
+        title: '協調性'
+      },
+      neuroticism: {
+        title: '神経症的性格'
+      }
+    },
+    common: {
+      and: 'そして',
+      pages: 'ページ',
+      languages: '言語'
+    },
+    form: {
+      information: '情報',
+      informationText: '私たちはあなたにサービスを提供するためにあなたが提供した情報を使用します',
+      mostAccurate: '可能な限り最も正確な結果',
+      readMoreAbout: 'についてもっと読む',
+      privacy: 'プライバシー',
+      nextButton: 'オーケー、次の質問',
+      declineButton: '共有せずに続行',
+      prefferedLanguage: '私の好きな言語は',
+      selectLanguage: '言語を選択してください',
+      iama: '私は',
+      male: '男性',
+      female: '女性',
+      age: '年齢',
+      iam: 'わたし',
+      yearsOld: '年',
+      ageWarning: '16歳以上である必要があります',
+      language: '言語',
+      gender: '性別',
+      confirmInfo: '情報を確認する',
+      confirm: '確認',
+      toTestButton: '了解しました。テストに連れて行ってください'
+    },
+    test: {
+      next: '次',
+      back: 'バック',
+      more: 'もっと',
+      seeResults: '結果を見る'
+    },
+    big_five: {
+      title: 'ビッグファイブは何ですか？',
+      seo: {
+        title: 'ビッグファイブは何ですか？',
+        description: 'ビッグファイブについてもっと読む'
+      }
+    },
+    openness_to_experience: {
+      title: '経験への開放性',
+      seo: {
+        title: 'ドメインの経験への開放性の概要。',
+        description: 'b5モデルのOpennessToExperienceドメインの詳細を読む'
+      }
+    },
+    conscientiousness: {
+      title: '誠実性',
+      seo: {
+        title: 'ドメインの概要誠実性',
+        description: 'b5モデルの誠実性ドメインについてもっと読む'
+      }
+    },
+    extraversion: {
+      title: '外向性',
+      seo: {
+        title: 'ドメイン外向性の概要',
+        description: 'b5モデルの外向性ドメインについてもっと読む'
+      }
+    },
+    agreeableness: {
+      title: '協調性',
+      seo: {
+        title: 'ドメインの概要',
+        description: 'b5モデルの協調性ドメインについてもっと読む'
+      }
+    },
+    neuroticism: {
+      title: '神経症的性格',
+      seo: {
+        title: 'ドメイン神経症的性格の概要',
+        description: 'b5モデルの神経症的性格ドメインについてもっと読む'
+      }
+    },
+    getCompare: {
+      title: '人やチームを比較する',
+      description1: 'ビッグファイブの性格検査の結果を複数の人と比較します。',
+      description2: 'テスト結果から取得したIDを入力します。',
+      description3: 'ID入力フィールド',
+      needToAddPeople: 'あなたはそれらを比較するために人々を追加する必要があります',
+      addAnother: '別の人を追加する',
+      addPerson: '人を追加',
+      comparePeople: '人を比較する',
+      nameOfPerson: 'この人の名前',
+      urlOrId: '比較のためのURLまたはID',
+      name: '名前'
+    },
+    results: {
+      theBigFive: 'ビッグファイブ',
+      score: 'スコア',
+      important: '重要！',
+      saveResults: '次のIDを保存して、後で結果を確認するか、 ',
+      compare: '比較する',
+      toOthers: '他人に自分自身',
+      seo: {
+        title: 'BigFive性格検査の結果',
+        description: '私の性格を見て、次の5つのドメインで自分自身を比較してください：神経症的性格、経験への開放性、誠実性、外向性、協調性'
+      }
+    },
+    getResult: {
+      result: '結果',
+      explaination: 'テストを受けてIDを保存した場合は、取得したIDを入力すると、ここに結果が表示されます。',
+      idInput: 'ID入力フィールド',
+      urlOrId: '結果のURLまたはID',
+      getResult: '結果を得る'
+    },
+    shareLinks: {
+      copyLink: 'リンクをクリップボードにコピーする',
+      shareFacebook: 'Facebookでシェア',
+      shareTwitter: 'Twitterで共有する',
+      copiedLink: 'コピーされたリンク',
+      shareResults: '結果を共有してください！'
+    },
+    opennessToExperience: {
+      quote: 'Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.',
+      text1: `
+        <p>
+          Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.
+        </p>
+        <p>
+          Open people are <b>intellectually curious, appreciative of art, and sensitive to beauty</b>.
+          They tend to be, compared to closed people, more <b>aware of their feelings</b>.
+          They tend to think and act in individualistic and nonconforming ways.
+        </p>
+        <p>
+          Intellectuals typically score high on Openness to Experience; consequently, this factor has also been called Culture or Intellect.
+        </p>
+      `,
+      text2: `
+        <p>
+          Nonetheless, Intellect is probably best regarded as one aspect of openness to experience.
+          Scores on Openness to Experience are only modestly related to years of education and scores on standard intelligent tests.
+        </p>
+        <p>
+          Another characteristic of the open cognitive style is a facility for thinking in symbols and abstractions far removed from concrete experience.
+        </p>
+        <p>
+          Depending on the individual's specific intellectual abilities, this symbolic cognition may take the form of mathematical, logical, or geometric thinking, artistic and metaphorical use of language, music composition or performance, or one of the many visual or performing arts.
+        </p>
+        <div class="title">
+          Low scores
+        </div>
+        <p>
+          People with <b>low scores</b> on openness to experience tend to have <b>narrow, common interests</b>.
+        </p>
+        <p>
+          They prefer the <b>plain, straightforward</b>, and obvious over the complex, ambiguous, and subtle.
+        </p>
+        <p>
+          They may regard the arts and sciences with suspicion, regarding these endeavors as abstruse or of no practical use.
+        </p>
+        <p>
+          Closed people prefer familiarity over novelty; they are conservative and resistant to change.
+        </p>
+        <p>
+          Openness is often presented as healthier or more mature by psychologists, who are often themselves open to experience. However, open and closed styles of thinking are useful in different environments.
+        </p>
+        <p>
+        The intellectual style of the open person may serve a professor well, but research has shown that closed thinking is related to superior job performance in police work, sales, and a number of service occupations.
+        </p>
+  
+        <div class="title">
+          Imagination
+        </div>
+        <p>
+          To imaginative individuals, the real world is often too plain and ordinary.
+        </p>
+        <p>
+          <b>High scorers</b> on this scale use fantasy as a way of creating a richer, more interesting world.
+        </p>
+        <p>
+        <b>Low scorers</b> are on this scale are more oriented to facts than fantasy.
+        </p>
+  
+        <div class="title">
+          Artistic Interests
+        </div>
+        <p>
+        <b>High scorers</b> on this scale love beauty, both in art and in nature. They become easily involved and absorbed in artistic and natural events.
+        </p>
+        <p>
+          They are not necessarily artistically trained nor talented, although many will be. The defining features of this scale are interest in, and appreciation of natural and artificial beauty.
+        </p>
+        <p>
+          <b>Low scorers</b> lack aesthetic sensitivity and interest in the arts.
+        </p>
+  
+        <div class="title">
+          Emotionality
+        </div>
+        <p>
+          Persons high on Emotionality have good access to and awareness of their own feelings.
+        </p>
+        <p>
+          Low scorers are less aware of their feelings and tend not to express their emotions openly.
+        </p>
+  
+        <div class="title">
+          Adventurousness
+        </div>
+        <p>
+          High scorers on adventurousness are eager to try new activities, travel to foreign lands, and experience different things.
+        </p>
+        <p>
+          They find familiarity and routine boring, and will take a new route home just because it is different.
+        </p>
+        <p>
+          Low scorers tend to feel uncomfortable with change and prefer familiar routines.
+        </p>
+  
+        <div class="title">
+          Intellect
+        </div>
+        <p>
+          Intellect and artistic interests are the two most important, central aspects of openness to experience. High scorers on Intellect love to play with ideas.
+        </p>
+        <p>
+          They are open-minded to new and unusual ideas, and like to debate intellectual issues. They enjoy riddles, puzzles, and brain teasers. Low scorers on Intellect prefer dealing with either people or things rather than ideas.
+        </p>
+        <p>
+          They regard intellectual exercises as a waste of time.
+        </p>
+        <p>
+          Intellect should not be equated with intelligence. Intellect is an intellectual style, not an intellectual ability, although high scorers on Intellect score slightly higher than low-Intellect individuals on standardized intelligence tests.
+        </p>
+  
+        <div class="title">
+          Liberalism
+        </div>
+        <p>
+          Psychological liberalism refers to a readiness to challenge authority, convention, and traditional values.
+        </p>
+        <p>
+          In its most extreme form, psychological liberalism can even represent outright hostility toward rules, sympathy for law-breakers, and love of ambiguity, chaos, and disorder.
+        </p>
+        <p>
+          Psychological conservatives prefer the security and stability brought by conformity to tradition. Psychological liberalism and conservatism are not identical to political affiliation, but certainly incline individuals toward certain political parties.
+        </p>
+      `
+    }
+  }
+  
