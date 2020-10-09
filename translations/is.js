cb6e4c793697ab0bd39cdd1d211cf7e687e4e097
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-09 20:18:17 +0200

    lintfix

--- translations/is.js
+++ translations/is.js
@@ -1,205 +1,205 @@
 export default {
-    language: 'Icelandic',
+  language: 'Icelandic',
+  seo: {
+    keywords: 'stór fimm persónuleikapróf, stór 5 persónuleikapróf, b5 próf, bigfive próf, persónueinkenni, bigfive, bera saman, ókeypis, Jordan Peterson '
+  },
+  frontpage: {
     seo: {
-      keywords: 'stór fimm persónuleikapróf, stór 5 persónuleikapróf, b5 próf, bigfive próf, persónueinkenni, bigfive, bera saman, ókeypis, Jordan Peterson '
+      title: 'Ókeypis opið uppspretta BigFive persónueinkenni próf',
+      description: 'Taktu ókeypis opið uppsprettu Big Five persónuleikapróf. Lærðu að þekkja persónueinkenni þín og berðu þig saman við maka þinn, samstarfsmenn, vini eða fjölskyldu.'
     },
-    frontpage: {
-      seo: {
-        title: 'Ókeypis opið uppspretta BigFive persónueinkenni próf',
-        description: 'Taktu ókeypis opið uppsprettu Big Five persónuleikapróf. Lærðu að þekkja persónueinkenni þín og berðu þig saman við maka þinn, samstarfsmenn, vini eða fjölskyldu.'
+    title: 'Stór fimm persónuleikapróf',
+    call_to_action: 'Taktu ókeypis prófið núna',
+    no_registration: '* Engin skráning nauðsynleg',
+    tests_taken: 'Yfir 700.000 manns hafa tekið prófið',
+    compare: {
+      title: 'Berðu þig saman við aðra',
+      text1: 'Berðu þig saman við maka þinn, samstarfsmenn, vini eða fjölskyldu.',
+      text2: 'Lærðu hvernig þú ert frábrugðin öðru fólki á lénunum fimm:',
+      action: 'Sjáðu það í beinni útsendingu'
+    },
+    share: 'Deildu með vinum þínum og komdu að því hversu samhæfur þú ert',
+    cards: {
+      open: {
+        title: 'Opið',
+        text: 'Þetta er opinn uppspretta verkefni undir MIT-leyfi.'
       },
-      title: 'Stór fimm persónuleikapróf',
-      call_to_action: 'Taktu ókeypis prófið núna',
-      no_registration: '* Engin skráning nauðsynleg',
-      tests_taken: 'Yfir 700.000 manns hafa tekið prófið',
-      compare: {
-        title: 'Berðu þig saman við aðra',
-        text1: 'Berðu þig saman við maka þinn, samstarfsmenn, vini eða fjölskyldu.',
-        text2: 'Lærðu hvernig þú ert frábrugðin öðru fólki á lénunum fimm:',
-        action: 'Sjáðu það í beinni útsendingu'
+      free: {
+        title: 'Ókeypis',
+        text: 'Prófið er alveg ókeypis'
       },
-      share: 'Deildu með vinum þínum og komdu að því hversu samhæfur þú ert',
-      cards: {
-        open: {
-          title: 'Opið',
-          text: 'Þetta er opinn uppspretta verkefni undir MIT-leyfi.'
-        },
-        free: {
-          title: 'Ókeypis',
-          text: 'Prófið er alveg ókeypis'
-        },
-        scientific: {
-          title: 'Vísindalegt',
-          text: 'BigFive er vísindalega fullgilt og áreiðanlegt sálfræðilegt líkan.'
-        },
-        translated: {
-          title: 'Translated',
-          text: 'Þýtt á yfir 20 tungumál. Hjálpaðu <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank"> hér </a>!'
-        }
+      scientific: {
+        title: 'Vísindalegt',
+        text: 'BigFive er vísindalega fullgilt og áreiðanlegt sálfræðilegt líkan.'
       },
-      description: {
-        top: 'Lærðu að þekkja sjálfan þig betur með ókeypis opnu persónuleikaprófi.',
-        subtop: 'The',
-        subtop2: ' er vísindalega staðfestasta og áreiðanlegasta sálfræðilíkanið til að mæla persónuleika.',
-        reference: `Próf og mat fyrir þessa síðu er safnað saman frá
+      translated: {
+        title: 'Translated',
+        text: 'Þýtt á yfir 20 tungumál. Hjálpaðu <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank"> hér </a>!'
+      }
+    },
+    description: {
+      top: 'Lærðu að þekkja sjálfan þig betur með ókeypis opnu persónuleikaprófi.',
+      subtop: 'The',
+      subtop2: ' er vísindalega staðfestasta og áreiðanlegasta sálfræðilíkanið til að mæla persónuleika.',
+      reference: `Próf og mat fyrir þessa síðu er safnað saman frá
         <a href="http://ipip.ori.org" rel="noopener" target="_blank"> ipip.ori.org </a>,
         birgðir eru frá <i> Johnson (2014) 120 hlutum IPIP NEO-PI-R </i>.`,
-        info: 'Eftirfarandi próf inniheldur 120 spurningar sem áætlað er að taki þig um það bil 10 mínútur.',
-        result: 'Eftir að prófinu lýkur færðu nákvæma persónuleikaskýrslu á eftirfarandi sviðum:',
-        tests_taken: 'próf sem tekin hafa verið hingað til'
-      }
-    },
-    about: {
-      seo: {
-        title: 'Um og fimm stóra liðið',
-        description: 'BigFive er með mjög virkt og trúlofað lið sem er stöðugt að reyna að ýta BigFive áfram.'
-      }
-    },
-    toolbar: {
-      home: 'Heim',
-      result: 'Niðurstaða',
-      compare: 'Berðu saman',
-      articles: 'Greinar',
-      privacy: 'Persónuvernd',
-      about: 'Um það bil',
-      see_results: 'Sjáðu niðurstöðurnar þínar',
-      compare_with: 'Berðu saman við aðra'
-    },
-    facets: {
-      openness_to_experience: {
-        title: 'Opinberð að upplifa'
-      },
-      conscientiousness: {
-        title: 'Samviskusemi'
-      },
-      extraversion: {
-        title: 'Öfugugni'
-      },
-      agreeableness: {
-        title: 'Agreeableness'
-      },
-      neuroticism: {
-        title: 'Taugaveiki'
-      }
-    },
-    common: {
-      and: 'og',
-      pages: 'blaðsíður',
-      languages: 'tungumálum'
-    },
-    form: {
-      information: 'Upplýsingar',
-      informationText: 'Við notum upplýsingarnar sem þú gefur til að þjóna þér',
-      mostAccurate: 'nákvæmasta niðurstaðan möguleg',
-      readMoreAbout: 'Lestu meira um',
-      privacy: 'næði',
-      nextButton: 'Ókey, næsta spurning',
-      declineButton: 'halda áfram án þess að deila',
-      prefferedLanguage: 'Æskilegt tungumál mitt er',
-      selectLanguage: 'Veldu tungumál',
-      iama: 'Ég er',
-      male: 'Karlkyns',
-      female: 'Kvenkyns',
-      age: 'Aldur',
-      iam: 'ég er',
-      yearsOld: 'ára',
-      ageWarning: 'Þú verður að vera 16 ára eða eldri',
-      language: 'Tungumál',
-      gender: 'Kyn',
-      confirmInfo: 'Staðfestu upplýsingar',
-      confirm: 'Staðfesta',
-      toTestButton: 'Allt í lagi, prófaðu mig'
-    },
-    test: {
-      next: 'næst',
-      back: 'aftur',
-      more: 'meira',
-      seeResults: 'sjá niðurstöður'
-    },
-    big_five: {
-      title: 'Hverjar eru fimm stóru?',
-      seo: {
-        title: 'Hverjar eru fimm stóru?',
-        description: 'Lestu meira um stóru fimm'
-      }
-    },
+      info: 'Eftirfarandi próf inniheldur 120 spurningar sem áætlað er að taki þig um það bil 10 mínútur.',
+      result: 'Eftir að prófinu lýkur færðu nákvæma persónuleikaskýrslu á eftirfarandi sviðum:',
+      tests_taken: 'próf sem tekin hafa verið hingað til'
+    }
+  },
+  about: {
+    seo: {
+      title: 'Um og fimm stóra liðið',
+      description: 'BigFive er með mjög virkt og trúlofað lið sem er stöðugt að reyna að ýta BigFive áfram.'
+    }
+  },
+  toolbar: {
+    home: 'Heim',
+    result: 'Niðurstaða',
+    compare: 'Berðu saman',
+    articles: 'Greinar',
+    privacy: 'Persónuvernd',
+    about: 'Um það bil',
+    see_results: 'Sjáðu niðurstöðurnar þínar',
+    compare_with: 'Berðu saman við aðra'
+  },
+  facets: {
     openness_to_experience: {
-      title: 'Opinberð til reynslu',
-      seo: {
-        title: 'Yfirlit yfir lénið Openness To Experience.',
-        description: 'Lestu meira um lénið Openness To Experience í b5 líkaninu'
-      }
+      title: 'Opinberð að upplifa'
     },
     conscientiousness: {
-      title: 'Samviskusemi',
-      seo: {
-        title: 'Yfirlit yfir lénið Samviskusemi',
-        description: 'Lestu meira um Conscientiousness lénið í b5 líkaninu'
-      }
+      title: 'Samviskusemi'
     },
     extraversion: {
-      title: 'Öfugugni',
-      seo: {
-        title: 'Yfirlit yfir lénið Extraversion',
-        description: 'Lestu meira um Extraversion lénið í b5 gerðinni'
-      }
+      title: 'Öfugugni'
     },
     agreeableness: {
-      title: 'Samþykkt',
-      seo: {
-        title: 'Yfirlit yfir lénið Samþykki',
-        description: 'Lestu meira um þægindalén í b5 líkaninu'
-      }
+      title: 'Agreeableness'
     },
     neuroticism: {
-      title: 'Taugaveiki',
-      seo: {
-        title: 'Yfirlit yfir lénið Neuroticism',
-        description: 'Lestu meira um taugakerfissviðið í b5 líkaninu'
-      }
-    },
-    getCompare: {
-      title: 'Berðu fólk eða lið saman',
-      description1: 'Berðu saman niðurstöður úr bigfive persónuleikaprófinu við marga.',
-      description2: 'Sláðu inn skilríkin sem þú fékkst frá niðurstöðum prófanna þ.e.a.s.',
-      description3: 'í auðkennisreitinn',
-      needToAddPeople: 'Þú verður að bæta við fólki til að bera það saman',
-      addAnother: 'Bættu við annarri manneskju',
-      addPerson: 'Bæta við einstaklingi',
-      comparePeople: 'bera fólk saman',
-      nameOfPerson: 'Nafn þessarar manneskju',
-      urlOrId: 'Slóð eða auðkenni til samanburðar',
-      name: 'Nafn'
-    },
-    results: {
-      theBigFive: 'Fimm stóru',
-      score: 'mark',
-      important: 'Mikilvægt!',
-      saveResults: 'Vistaðu eftirfarandi auðkenni til að sjá niðurstöðurnar síðar eða',
-      compare: 'bera saman',
-      toOthers: 'sjálfur við aðra',
-      seo: {
-        title: 'Niðurstöður mínar úr BigFive persónuleikaprófinu',
-        description: 'Sjáðu persónuleika minn og berðu þig saman á eftirfarandi fimm sviðum: Taugaveiki, hreinskilni til að upplifa, samviskusemi, aukaatriði, samþykki'
-      }
-    },
-    getResult: {
-      result: 'Niðurstaða',
-      explaination: 'Ef þú hefur tekið prófið og vistað skilríkin geturðu séð niðurstöðurnar hér með því að slá inn skilríkin sem þú fékkst þ.e.a.s. ',
-      idInput: 'í auðkennisreitinn',
-      urlOrId: 'Slóð eða auðkenni niðurstöðu þinnar',
-      getResult: 'Fáðu niðurstöðu'
-    },
-    shareLinks: {
-      copyLink: 'Afritaðu krækju á klemmuspjald',
-      shareFacebook: 'Deildu á Facebook',
-      shareTwitter: 'Deildu á Twitter',
-      copiedLink: 'Afritaður hlekkur',
-      shareResults: 'Deildu niðurstöðum þínum!'
-    },
-    opennessToExperience: {
-      quote: 'Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.',
-      text1: `
+      title: 'Taugaveiki'
+    }
+  },
+  common: {
+    and: 'og',
+    pages: 'blaðsíður',
+    languages: 'tungumálum'
+  },
+  form: {
+    information: 'Upplýsingar',
+    informationText: 'Við notum upplýsingarnar sem þú gefur til að þjóna þér',
+    mostAccurate: 'nákvæmasta niðurstaðan möguleg',
+    readMoreAbout: 'Lestu meira um',
+    privacy: 'næði',
+    nextButton: 'Ókey, næsta spurning',
+    declineButton: 'halda áfram án þess að deila',
+    prefferedLanguage: 'Æskilegt tungumál mitt er',
+    selectLanguage: 'Veldu tungumál',
+    iama: 'Ég er',
+    male: 'Karlkyns',
+    female: 'Kvenkyns',
+    age: 'Aldur',
+    iam: 'ég er',
+    yearsOld: 'ára',
+    ageWarning: 'Þú verður að vera 16 ára eða eldri',
+    language: 'Tungumál',
+    gender: 'Kyn',
+    confirmInfo: 'Staðfestu upplýsingar',
+    confirm: 'Staðfesta',
+    toTestButton: 'Allt í lagi, prófaðu mig'
+  },
+  test: {
+    next: 'næst',
+    back: 'aftur',
+    more: 'meira',
+    seeResults: 'sjá niðurstöður'
+  },
+  big_five: {
+    title: 'Hverjar eru fimm stóru?',
+    seo: {
+      title: 'Hverjar eru fimm stóru?',
+      description: 'Lestu meira um stóru fimm'
+    }
+  },
+  openness_to_experience: {
+    title: 'Opinberð til reynslu',
+    seo: {
+      title: 'Yfirlit yfir lénið Openness To Experience.',
+      description: 'Lestu meira um lénið Openness To Experience í b5 líkaninu'
+    }
+  },
+  conscientiousness: {
+    title: 'Samviskusemi',
+    seo: {
+      title: 'Yfirlit yfir lénið Samviskusemi',
+      description: 'Lestu meira um Conscientiousness lénið í b5 líkaninu'
+    }
+  },
+  extraversion: {
+    title: 'Öfugugni',
+    seo: {
+      title: 'Yfirlit yfir lénið Extraversion',
+      description: 'Lestu meira um Extraversion lénið í b5 gerðinni'
+    }
+  },
+  agreeableness: {
+    title: 'Samþykkt',
+    seo: {
+      title: 'Yfirlit yfir lénið Samþykki',
+      description: 'Lestu meira um þægindalén í b5 líkaninu'
+    }
+  },
+  neuroticism: {
+    title: 'Taugaveiki',
+    seo: {
+      title: 'Yfirlit yfir lénið Neuroticism',
+      description: 'Lestu meira um taugakerfissviðið í b5 líkaninu'
+    }
+  },
+  getCompare: {
+    title: 'Berðu fólk eða lið saman',
+    description1: 'Berðu saman niðurstöður úr bigfive persónuleikaprófinu við marga.',
+    description2: 'Sláðu inn skilríkin sem þú fékkst frá niðurstöðum prófanna þ.e.a.s.',
+    description3: 'í auðkennisreitinn',
+    needToAddPeople: 'Þú verður að bæta við fólki til að bera það saman',
+    addAnother: 'Bættu við annarri manneskju',
+    addPerson: 'Bæta við einstaklingi',
+    comparePeople: 'bera fólk saman',
+    nameOfPerson: 'Nafn þessarar manneskju',
+    urlOrId: 'Slóð eða auðkenni til samanburðar',
+    name: 'Nafn'
+  },
+  results: {
+    theBigFive: 'Fimm stóru',
+    score: 'mark',
+    important: 'Mikilvægt!',
+    saveResults: 'Vistaðu eftirfarandi auðkenni til að sjá niðurstöðurnar síðar eða',
+    compare: 'bera saman',
+    toOthers: 'sjálfur við aðra',
+    seo: {
+      title: 'Niðurstöður mínar úr BigFive persónuleikaprófinu',
+      description: 'Sjáðu persónuleika minn og berðu þig saman á eftirfarandi fimm sviðum: Taugaveiki, hreinskilni til að upplifa, samviskusemi, aukaatriði, samþykki'
+    }
+  },
+  getResult: {
+    result: 'Niðurstaða',
+    explaination: 'Ef þú hefur tekið prófið og vistað skilríkin geturðu séð niðurstöðurnar hér með því að slá inn skilríkin sem þú fékkst þ.e.a.s. ',
+    idInput: 'í auðkennisreitinn',
+    urlOrId: 'Slóð eða auðkenni niðurstöðu þinnar',
+    getResult: 'Fáðu niðurstöðu'
+  },
+  shareLinks: {
+    copyLink: 'Afritaðu krækju á klemmuspjald',
+    shareFacebook: 'Deildu á Facebook',
+    shareTwitter: 'Deildu á Twitter',
+    copiedLink: 'Afritaður hlekkur',
+    shareResults: 'Deildu niðurstöðum þínum!'
+  },
+  opennessToExperience: {
+    quote: 'Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.',
+    text1: `
         <p>
           Openness to Experience describes a dimension of cognitive style that distinguishes imaginative, creative people from down-to-earth, conventional people.
         </p>
@@ -212,7 +212,7 @@ export default {
           Intellectuals typically score high on Openness to Experience; consequently, this factor has also been called Culture or Intellect.
         </p>
       `,
-      text2: `
+    text2: `
         <p>
           Nonetheless, Intellect is probably best regarded as one aspect of openness to experience.
           Scores on Openness to Experience are only modestly related to years of education and scores on standard intelligent tests.
@@ -323,6 +323,5 @@ export default {
           Psychological conservatives prefer the security and stability brought by conformity to tradition. Psychological liberalism and conservatism are not identical to political affiliation, but certainly incline individuals toward certain political parties.
         </p>
       `
-    }
   }
-  
+}
